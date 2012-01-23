5cabe56bf39d1acccf644d110d13dfbd095af355
Will Drewry <wad@chromium.org>
2012-01-23 14:47:47 -0600

    verity-static: bring back static compilation for verity
    This common.mk is old, but the basics of this are good. This shows how to get
    all your static libraries dynamically and link them.  The trick is picking PIC
    over PIE since toolchains rarely ship with PIE components.  If we havepie
    archives, we should use them :)
    
    TEST=builds and runs
    BUG=none
    
    Change-Id: I070d4eff8fb3bdc604ea67ee3f67dac50d03740e
    Reviewed-on: https://gerrit.chromium.org/gerrit/14631
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Tested-by: Will Drewry <wad@chromium.org>
    Commit-Ready: Will Drewry <wad@chromium.org>

--- verity/simple_file/module.mk
+++ verity/simple_file/module.mk
@@ -7,7 +7,13 @@ include common.mk
 $(OUT)simple_file/libsimple_file-pie.a: $(simple_file_CXX_OBJECTS)
 	$(call update_archive,pie)
 
+$(OUT)simple_file/libsimple_file-pic.a: $(simple_file_CXX_OBJECTS)
+	$(call update_archive,pic)
+
+
 # Add our target
 all: $(OUT)simple_file/libsimple_file-pie.a
+all: $(OUT)simple_file/libsimple_file-pic.a
 # Ditto
 RM_ON_CLEAN += $(OUT)simple_file/libsimple_file-pie.a
+RM_ON_CLEAN += $(OUT)simple_file/libsimple_file-pic.a
