47096e1568df57ced90291ca2567dd604f3dfdd1
Joao da Silva <joaodasilva@chromium.org>
2014-10-22 14:44:06 +0200

    login: Increased the enrollment modulus bits.
    13 bits will now be uploaded by default, up to a maximum of 17 bits.
    
    BUG=chromium:425975
    TEST=None
    
    Reviewed-on: https://chromium-review.googlesource.com/224950
    Tested-by: Joao da Silva <joaodasilva@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Joao da Silva <joaodasilva@chromium.org>
    
    (cherry picked from commit 126093bd3902fc1ae54a168c54f38b348cd9c643)
    
    Change-Id: I167d85dcf093ec64f47d27a9ba9abd41e85041cb
    Reviewed-on: https://chromium-review.googlesource.com/224945
    Reviewed-by: Joao da Silva <joaodasilva@chromium.org>
    Commit-Queue: Joao da Silva <joaodasilva@chromium.org>
    Tested-by: Joao da Silva <joaodasilva@chromium.org>

--- login_manager/chrome_setup.cc
+++ login_manager/chrome_setup.cc
@@ -241,8 +241,8 @@ void AddEnterpriseFlags(ChromiumCommandBuilder* builder) {
       "--device-management-url=https://m.google.com/devicemanagement/data/api");
 
   builder->AddArg("--enterprise-enable-forced-re-enrollment");
-  builder->AddArg("--enterprise-enrollment-initial-modulus=12");
-  builder->AddArg("--enterprise-enrollment-modulus-limit=16");
+  builder->AddArg("--enterprise-enrollment-initial-modulus=13");
+  builder->AddArg("--enterprise-enrollment-modulus-limit=17");
 }
 
 // Adds patterns to the --vmodule flag.
