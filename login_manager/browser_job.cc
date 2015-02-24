037b015898c361de5b155ddd21691d22b5d45d91
Chris Masone <cmasone@chromium.org>
2015-02-24 09:26:34 -0800

    login: Wait for all descendants in browser process group
    Since kernel 3.4, it's been possible for a process to wait on all
    descendants, not just direct children, given the appropriate prctl().
    
    The session_manager has always wanted to do this, so let's do it!
    
    BUG=chromium:436549
    TEST=bvt-inline, unittests
    
    Reviewed-on: https://chromium-review.googlesource.com/252803
    Trybot-Ready: Chris Masone <cmasone@chromium.org>
    Tested-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Chris Masone <cmasone@chromium.org>
    (cherry picked from commit 1ebe027077ef6a2e0c090143bc5179a7f3531e62)
    
    Change-Id: Ic8e9a45688f0972639d7523eb22c4d73b73c65ec
    Reviewed-on: https://chromium-review.googlesource.com/253620
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Tested-by: Yuly Novikov <ynovikov@chromium.org>

--- login_manager/browser_job.cc
+++ login_manager/browser_job.cc
@@ -130,12 +130,12 @@ void BrowserJob::Kill(int signal, const std::string& message) {
 void BrowserJob::WaitAndAbort(base::TimeDelta timeout) {
   if (subprocess_.pid() < 0)
     return;
-  if (!system_->ChildIsGone(subprocess_.pid(), timeout)) {
+  if (!system_->ProcessGroupIsGone(subprocess_.pid(), timeout)) {
     LOG(WARNING) << "Aborting child process " << subprocess_.pid()
                  << "'s process group " << timeout.InSeconds()
-                 << " seconds after sending TERM signal";
+                 << " seconds after sending signal";
     std::string message = base::StringPrintf("Browser took more than %" PRId64
-                                             " seconds to exit after TERM.",
+                                             " seconds to exit after signal.",
                                              timeout.InSeconds());
     KillEverything(SIGABRT, message);
   } else {
