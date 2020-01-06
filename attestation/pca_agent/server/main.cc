11f75059af83022265ae437216ac9f8331d2466c
Leo Lai <cylai@google.com>
2020-01-06 11:52:52 +0800

    attestation: minijail for pca_agentd
    BUG=b:149723745
    TEST=build OK.
    TEST=run with the implementation of pca_agentd.
    
    Change-Id: I1511067b1935216c157b8f03f8b9cae81e0436c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1990531
    Tested-by: Leo Lai <cylai@google.com>
    Commit-Queue: Leo Lai <cylai@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- attestation/pca_agent/server/main.cc
+++ attestation/pca_agent/server/main.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <unistd.h>
-
 #include <base/command_line.h>
 #include <brillo/syslog_logging.h>
 
@@ -17,8 +15,6 @@ int main(int argc, char* argv[]) {
     flags |= brillo::kLogToStderr;
   }
   brillo::InitLog(flags);
-
-  PLOG_IF(FATAL, ::daemon(0, 0) == -1) << "Failed to daemonize";
 
   return attestation::pca_agent::PcaAgentDaemon().Run();
 }
