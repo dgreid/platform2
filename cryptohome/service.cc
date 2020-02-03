846ef3bbe9165f3fe391c88c434a6ff375055bd6
Leo Lai <cylai@google.com>
2020-02-03 13:43:25 +0800

    cryptohome: try reset da first then check PCR0 value
    Since we removed PCR locality of owner delegate for TPM1.2, the da reset
    should not assume the wrong PCR0 value would always fails the da reset
    operations.
    
    Thus, check the PCR0 value only when the operation really fails.
    
    BUG=b:148757044
    TEST=unittest
    TEST=reset da working on real device
    
    Change-Id: I078c4c4fb6c3d6909663d7bbcabeb9398fac380e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2032963
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Tested-by: Leo Lai <cylai@google.com>
    Commit-Queue: Leo Lai <cylai@google.com>

--- cryptohome/service.cc
+++ cryptohome/service.cc
@@ -3629,12 +3629,12 @@ void Service::ResetDictionaryAttackMitigation() {
     ReportDictionaryAttackResetStatus(kDelegateNotAllowed);
     return;
   }
-  if (!tpm_->IsCurrentPCR0ValueValid()) {
-    ReportDictionaryAttackResetStatus(kInvalidPcr0State);
-    return;
-  }
   if (!tpm_->ResetDictionaryAttackMitigation(delegate_blob, delegate_secret)) {
-    ReportDictionaryAttackResetStatus(kResetAttemptFailed);
+    if (!tpm_->IsCurrentPCR0ValueValid()) {
+      ReportDictionaryAttackResetStatus(kInvalidPcr0State);
+    } else {
+      ReportDictionaryAttackResetStatus(kResetAttemptFailed);
+    }
     return;
   }
   ReportDictionaryAttackResetStatus(kResetAttemptSucceeded);
