f1119273f24f6df21bbe86d98d13eed20f0f7738
John L Chen <zuan@chromium.org>
2020-02-18 19:34:28 +0800

    cryptohome: Add logging to syslog for cryptohome-proxy
    BUG=none
    TEST=unit tests && manually check logs are in /var/log/messages
    
    Change-Id: I45cd74fa6b94795ab89f9e51f86b38d0e95ee90b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2061969
    Commit-Queue: John L Chen <zuan@chromium.org>
    Tested-by: John L Chen <zuan@chromium.org>
    Reviewed-by: Louis Collard <louiscollard@chromium.org>

--- cryptohome/proxy/cryptohome_proxy.cc
+++ cryptohome/proxy/cryptohome_proxy.cc
@@ -2,12 +2,26 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <base/command_line.h>
 #include <base/logging.h>
+#include <brillo/syslog_logging.h>
 
 #include "cryptohome/proxy/dbus_service.h"
 
 int main(int argc, char** argv) {
 #if USE_CRYPTOHOME_USERDATAAUTH_INTERFACE
+  // Initialize command line configuration early, as logging will require
+  // command line to be initialized
+  base::CommandLine::Init(argc, argv);
+
+  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+  int flags = brillo::kLogToSyslog;
+  if (cl->HasSwitch("log_to_stderr")) {
+    flags |= brillo::kLogToStderr;
+  }
+
+  brillo::InitLog(flags);
+
   cryptohome::CryptohomeProxyDaemon proxy_daemon;
   return proxy_daemon.Run();
 #else
