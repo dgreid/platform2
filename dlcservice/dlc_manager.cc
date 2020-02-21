04fda43a23bc479a0dc5484ab5fbeb4258d8f105
Amin Hassani <ahassani@chromium.org>
2020-02-21 11:31:14 -0800

    dlcservice: Fix the order of some includes files
    BUG=chromium:1039420
    TEST=CQ passes
    
    Change-Id: I9df811c1487dac1bb24dcc5debae94d25949c853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2068545
    Commit-Queue: Amin Hassani <ahassani@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    Commit-Queue: Andrew Lassalle <andrewlassalle@chromium.org>
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Auto-Submit: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: Andrew Lassalle <andrewlassalle@chromium.org>

--- dlcservice/dlc_manager.cc
+++ dlcservice/dlc_manager.cc
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "dlcservice/dlc_manager.h"
+
 #include <set>
 #include <utility>
 #include <vector>
-
-#include "dlcservice/dlc_manager.h"
 
 #include <base/strings/stringprintf.h>
 #include <base/strings/string_number_conversions.h>
--- dlcservice/utils_test.cc
+++ dlcservice/utils_test.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "dlcservice/utils.h"
-
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -16,6 +14,8 @@
 #include <base/files/scoped_temp_dir.h>
 #include <brillo/file_utils.h>
 #include <gtest/gtest.h>
+
+#include "dlcservice/utils.h"
 
 namespace dlcservice {
 
