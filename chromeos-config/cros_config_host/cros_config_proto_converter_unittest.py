ff3c1b7c720c73270d1da19ba74d58c4bcb14eaa
C Shapiro <shapiroc@chromium.org>
2020-02-28 11:01:37 -0600

    chromeos-config: Added base proto transform script
    This will be the new command that coverts proto config into normalized
    json payload for processing by cros_config_host
    
    BUG=None
    TEST=unit
    
    Change-Id: I2b31e2e973d1061a1a74a5cfd5c7af2a9040c854
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2080057
    Commit-Queue: C Shapiro <shapiroc@chromium.org>
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>

---
+++ chromeos-config/cros_config_host/cros_config_proto_converter_unittest.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# pylint: disable=module-missing-docstring,class-missing-docstring
+
+import os
+import sys
+
+# pylint: disable=wrong-import-position
+this_dir = os.path.dirname(__file__)
+sys.path.insert(0, this_dir)
+import cros_config_proto_converter
+sys.path.pop(0)
+
+from chromite.lib import cros_test_lib
+
+
+class ParseArgsTests(cros_test_lib.TestCase):
+
+  def testParseArgs(self):
+    argv = ['-f', 'files-root', '-c', 'project-config',
+            '-p', 'program-config', '-o', 'output', ]
+    args = cros_config_proto_converter.ParseArgs(argv)
+    self.assertEqual(args.files_root, 'files-root')
+    self.assertEqual(args.project_config, 'project-config')
+    self.assertEqual(args.program_config, 'program-config')
+    self.assertEqual(args.output, 'output')
+
+
+if __name__ == '__main__':
+  cros_test_lib.main(module=__name__)
