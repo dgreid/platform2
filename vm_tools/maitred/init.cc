1e15cd2bb803b63a35379116b76883defd031233
Daniel Verkamp <dverkamp@chromium.org>
2020-02-28 15:21:52 -0800

    vm_tools: maitred: use common/spawn_util.cc
    Deduplicate the common parts of maitred's Spawn function by using the
    existing vm_tools/common/spawn_util.cc Spawn() utility function.
    
    BUG=None
    TEST=deploy_termina on nami; verify that Crostini still works
    
    Change-Id: If469027ecd724cabbb990ec136c75d731b87d0b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2081212
    Tested-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-by: Stephen Barber <smbarber@chromium.org>

--- vm_tools/maitred/init.cc
+++ vm_tools/maitred/init.cc
@@ -50,6 +50,8 @@
 #include <base/strings/string_util.h>
 #include <base/time/time.h>
 
+#include "vm_tools/common/spawn_util.h"
+
 using std::string;
 
 namespace vm_tools {
@@ -364,77 +366,6 @@ constexpr struct {
     },
 };
 
-// Information about any errors that happen in the child process before the exec
-// call.  This is sent back to the parent process via a socket.
-struct __attribute__((packed)) ChildErrorInfo {
-  enum class Reason : uint8_t {
-    // Failed to set session id.
-    SESSION_ID = 0,
-    // Unable to open console.
-    CONSOLE = 1,
-    // Unable to set stdio fds.
-    STDIO_FD = 2,
-    // Unable to set environment variable.
-    SETENV = 3,
-    // Unable to reset signal handlers.
-    SIGNAL_RESET = 4,
-    // Failed to exec the requested program.
-    EXEC = 5,
-  };
-
-  union {
-    // If |reason| is STDIO_FD, the fd that we failed to dup.
-    int32_t fd;
-
-    // If |reason| is SETENV, then the child process will append the key and
-    // value of the environment variable pair that failed to this struct.  This
-    // value tells the parent process the length of the 2 strings, including the
-    // '\0' byte for each string.
-    uint16_t env_length;
-
-    // If |reason| is SIGNAL_RESET, the signal number for which we failed to set
-    // the default disposition.
-    int32_t signo;
-  } details;
-
-  // The errno value after the failed action.
-  int32_t err;
-
-  // Error reason.
-  Reason reason;
-};
-
-// Number of defined signals that the process could receive (not including
-// real time signals).
-constexpr int kNumSignals = 32;
-
-// Resets all signal handlers to the default.  This is called in child processes
-// immediately before exec-ing so that signals are not unexpectedly blocked.
-// Returns 0 if all signal handlers were successfully set to their default
-// dispositions.  Returns the signal number of the signal for which resetting
-// the signal handler failed, if any.  Callers should inspect errno for the
-// error.
-int ResetSignalHandlers() {
-  for (int signo = 1; signo < kNumSignals; ++signo) {
-    if (signo == SIGKILL || signo == SIGSTOP) {
-      // sigaction returns an error if we try to set the disposition of these
-      // signals to SIG_DFL.
-      continue;
-    }
-    struct sigaction act = {
-        .sa_handler = SIG_DFL,
-        .sa_flags = 0,
-    };
-    sigemptyset(&act.sa_mask);
-
-    if (sigaction(signo, &act, nullptr) != 0) {
-      return signo;
-    }
-  }
-
-  return 0;
-}
-
 // Recursively changes the owner and group for all files and directories in
 // |path| (including |path|) to |uid| and |gid|, respectively.
 bool ChangeOwnerAndGroup(base::FilePath path, uid_t uid, gid_t gid) {
@@ -458,188 +389,6 @@ bool ChangeOwnerAndGroup(base::FilePath path, uid_t uid, gid_t gid) {
   }
 
   return true;
-}
-
-// Performs various setup steps in the child process after calling fork() but
-// before calling exec(). |error_fd| should be a valid file descriptor for a
-// socket and will be used to send error information back to the parent process
-// if any of the setup steps fail.
-void DoChildSetup(const char* console,
-                  const std::map<string, string>& env,
-                  int error_fd) {
-  // Create a new session and process group.
-  if (setsid() == -1) {
-    struct ChildErrorInfo info = {
-        .err = errno,
-        .reason = ChildErrorInfo::Reason::SESSION_ID,
-    };
-
-    send(error_fd, &info, sizeof(info), MSG_NOSIGNAL);
-    _exit(errno);
-  }
-
-  // File descriptor for the child's stdio.
-  int fd = open(console, O_RDWR | O_NOCTTY);
-  if (fd < 0) {
-    struct ChildErrorInfo info = {
-        .err = errno,
-        .reason = ChildErrorInfo::Reason::CONSOLE,
-    };
-
-    send(error_fd, &info, sizeof(info), MSG_NOSIGNAL);
-    _exit(errno);
-  }
-
-  // Override the parent's stdio fds with the console fd.
-  for (int newfd = 0; newfd < 3; ++newfd) {
-    if (dup2(fd, newfd) < 0) {
-      struct ChildErrorInfo info = {
-          .details = {.fd = newfd},
-          .err = errno,
-          .reason = ChildErrorInfo::Reason::STDIO_FD,
-      };
-
-      send(error_fd, &info, sizeof(info), MSG_NOSIGNAL);
-      _exit(errno);
-    }
-  }
-
-  // Close the console fd, if necessary.
-  if (fd >= 3) {
-    close(fd);
-  }
-
-  // Set the umask back to a reasonable default.
-  umask(0022);
-
-  // Set the environment variables.
-  for (const auto& pair : env) {
-    if (setenv(pair.first.c_str(), pair.second.c_str(), 1) == 0) {
-      continue;
-    }
-
-    // Failed to set an environment variable.  Send the error back to the
-    // parent process.
-    uint16_t env_length = 0;
-    if (pair.first.size() + pair.second.size() + 2 <
-        std::numeric_limits<uint16_t>::max()) {
-      env_length =
-          static_cast<uint16_t>(pair.first.size() + pair.second.size() + 2);
-    }
-    struct ChildErrorInfo info = {
-        .details = {.env_length = env_length},
-        .err = errno,
-        .reason = ChildErrorInfo::Reason::SETENV,
-    };
-    send(error_fd, &info, sizeof(info), MSG_NOSIGNAL);
-
-    // Also send back the offending (key, value) pair if it's not too long.
-    // The pair is sent back in the format: <key>\0<value>\0.
-    if (env_length != 0) {
-      struct iovec iovs[] = {
-          {
-              .iov_base =
-                  static_cast<void*>(const_cast<char*>(pair.first.data())),
-              .iov_len = pair.first.size() + 1,
-          },
-          {
-              .iov_base =
-                  static_cast<void*>(const_cast<char*>(pair.second.data())),
-              .iov_len = pair.second.size() + 1,
-          },
-      };
-      struct msghdr hdr = {
-          .msg_name = nullptr,
-          .msg_namelen = 0,
-          .msg_iov = iovs,
-          .msg_iovlen = sizeof(iovs) / sizeof(iovs[0]),
-          .msg_control = nullptr,
-          .msg_controllen = 0,
-          .msg_flags = 0,
-      };
-      sendmsg(error_fd, &hdr, MSG_NOSIGNAL);
-    }
-    _exit(errno);
-  }
-
-  // Restore signal handlers and unblock all signals.
-  int signo = ResetSignalHandlers();
-  if (signo != 0) {
-    struct ChildErrorInfo info = {
-        .details = {.signo = signo},
-        .err = errno,
-        .reason = ChildErrorInfo::Reason::SIGNAL_RESET,
-    };
-
-    send(error_fd, &info, sizeof(info), MSG_NOSIGNAL);
-    _exit(errno);
-  }
-
-  // Unblock all signals.
-  sigset_t mask;
-  sigfillset(&mask);
-  sigprocmask(SIG_UNBLOCK, &mask, nullptr);
-}
-
-// Logs information about the error that occurred in the child process.
-void LogChildError(const struct ChildErrorInfo& child_info, int fd) {
-  const char* msg = nullptr;
-  switch (child_info.reason) {
-    case ChildErrorInfo::Reason::SESSION_ID:
-      msg = "Failed to set session id in child process: ";
-      break;
-    case ChildErrorInfo::Reason::CONSOLE:
-      msg = "Failed to open console in child process: ";
-      break;
-    case ChildErrorInfo::Reason::STDIO_FD:
-      msg = "Failed to setup stdio file descriptors in child process: ";
-      break;
-    case ChildErrorInfo::Reason::SETENV:
-      msg = "Failed to set environment variable in child process: ";
-      break;
-    case ChildErrorInfo::Reason::SIGNAL_RESET:
-      msg = "Failed to reset signal handler disposition in child process: ";
-      break;
-    case ChildErrorInfo::Reason::EXEC:
-      msg = "Failed to execute requested program in child process: ";
-      break;
-  }
-
-  LOG(ERROR) << msg << strerror(child_info.err);
-
-  if (child_info.reason == ChildErrorInfo::Reason::STDIO_FD) {
-    LOG(ERROR) << "Unable to dup console fd to " << child_info.details.fd;
-    return;
-  }
-
-  if (child_info.reason == ChildErrorInfo::Reason::SIGNAL_RESET) {
-    LOG(ERROR) << "Unable to set signal disposition for signal "
-               << child_info.details.signo << " to SIG_DFL";
-    return;
-  }
-
-  if (child_info.reason == ChildErrorInfo::Reason::SETENV &&
-      child_info.details.env_length > 0) {
-    auto buf = std::make_unique<char[]>(child_info.details.env_length + 1);
-    if (recv(fd, buf.get(), child_info.details.env_length, 0) !=
-        child_info.details.env_length) {
-      PLOG(ERROR) << "Unable to fetch error details from child process";
-      return;
-    }
-    buf[child_info.details.env_length] = '\0';
-
-    char* key = buf.get();
-    char* value = strchr(buf.get(), '\0');
-    if (value - key == child_info.details.env_length) {
-      LOG(ERROR) << "Missing value in SETENV error details";
-      return;
-    }
-
-    // Step over the nullptr at the end of |key|.
-    ++value;
-
-    LOG(ERROR) << "Unable to set " << key << " to " << value;
-  }
 }
 
 // Waits for all the processes in |pids| to exit.  Returns when all processes
@@ -1025,6 +774,10 @@ class Init::Worker {
   // Information about processes launched by this process.
   std::map<pid_t, ChildInfo> children_;
 
+  // File descriptor for "/dev/console".
+  // This is used for spawned processes when |use_console| is true.
+  base::ScopedFD console_fd_;
+
   DISALLOW_COPY_AND_ASSIGN(Worker);
 };
 
@@ -1045,6 +798,9 @@ void Init::Worker::Start() {
       signal_fd_.get(), base::BindRepeating(&Init::Worker::OnSignalReadable,
                                             base::Unretained(this)));
   CHECK(watcher_) << "Failed to watch SIGCHLD file descriptor";
+
+  console_fd_.reset(open("/dev/console", O_RDWR | O_NOCTTY));
+  PCHECK(console_fd_.is_valid()) << "Failed to open /dev/console";
 }
 
 void Init::Worker::Spawn(struct ChildInfo info,
@@ -1053,89 +809,27 @@ void Init::Worker::Spawn(struct ChildInfo info,
   DCHECK_GT(info.argv.size(), 0);
   DCHECK(launch_info);
 
-  // Build the argv.
-  std::vector<const char*> argv(info.argv.size());
-  std::transform(info.argv.begin(), info.argv.end(), argv.begin(),
-                 [](const string& arg) -> const char* { return arg.c_str(); });
-  argv.emplace_back(nullptr);
-
-  // Create a pair of sockets for communicating information about the child
-  // process setup.  If there was an error in any of the steps performed before
-  // running execvp, then the child process will send back a ChildErrorInfo
-  // struct with the error details over the socket.  If the execvp runs
-  // successful then the socket will automatically be closed (because of
-  // the SOCK_CLOEXEC flag) and the parent will read 0 bytes from its end
-  // of the socketpair.
-  int info_fds[2];
-  if (socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, info_fds) != 0) {
-    PLOG(ERROR) << "Failed to create socketpair for child process";
-    launch_info->status = ProcessStatus::FAILED;
-    return;
-  }
-
   // Block all signals before forking to prevent signals from arriving in the
   // child.
   sigset_t mask, omask;
   sigfillset(&mask);
   sigprocmask(SIG_BLOCK, &mask, &omask);
 
-  pid_t pid = fork();
-  if (pid < 0) {
-    PLOG(ERROR) << "Failed to fork";
+  int stdio_fds[3] = {-1, -1, -1};
+  if (info.use_console) {
+    for (auto& fd : stdio_fds) {
+      fd = console_fd_.get();
+    }
+  }
+
+  pid_t pid = -1;
+  bool spawned = vm_tools::Spawn(info.argv, info.env, "" /* working_dir */,
+                                 stdio_fds, &pid);
+
+  if (!spawned) {
+    LOG(ERROR) << "Failed to spawn child process";
     launch_info->status = ProcessStatus::FAILED;
-    return;
-  }
-
-  if (pid == 0) {
-    // Child process.
-    close(info_fds[0]);
-
-    const char* console = info.use_console ? "/dev/console" : "/dev/null";
-    DoChildSetup(console, info.env, info_fds[1]);
-
-    // Launch the process.
-    execvp(argv[0], const_cast<char* const*>(argv.data()));
-
-    // execvp never returns except in case of an error.
-    struct ChildErrorInfo info = {
-        .err = errno,
-        .reason = ChildErrorInfo::Reason::EXEC,
-    };
-
-    send(info_fds[1], &info, sizeof(info), MSG_NOSIGNAL);
-    _exit(errno);
-  }
-
-  // Parent process.
-  close(info_fds[1]);
-  struct ChildErrorInfo child_info = {};
-  ssize_t ret = recv(info_fds[0], &child_info, sizeof(child_info), 0);
-
-  // There are 3 possibilities here:
-  //   - The process setup completed successfully and the program was launched.
-  //     In this case the socket fd in the child process will be closed on
-  //     exec and ret will be 0.
-  //   - An error occurred during setup.  ret will be sizeof(child_info).
-  //   - An error occurred during the recv.  In this case we assume the child
-  //     setup was successful.  If it wasn't, we'll find out about it through
-  //     the normal child reaping mechanism.
-  if (ret == sizeof(child_info)) {
-    // Error occurred in the child.
-    LogChildError(child_info, info_fds[0]);
-
-    // Reap the child process here since we know it already failed.
-    int status = 0;
-    pid_t child = waitpid(pid, &status, 0);
-    DCHECK_EQ(child, pid);
-
-    launch_info->status = ProcessStatus::FAILED;
-  } else if (ret < 0) {
-    PLOG(ERROR) << "Failed to receive information about child process setup";
-    launch_info->status = ProcessStatus::UNKNOWN;
-  }
-  close(info_fds[0]);
-
-  if (ret == 0 && info.wait_for_exit) {
+  } else if (info.wait_for_exit) {
     int status = 0;
     pid_t child = waitpid(pid, &status, 0);
     DCHECK_EQ(child, pid);
@@ -1149,8 +843,7 @@ void Init::Worker::Spawn(struct ChildInfo info,
     } else {
       launch_info->status = ProcessStatus::UNKNOWN;
     }
-
-  } else if (ret == 0) {
+  } else {
     info.spawn_times.emplace_back(base::Time::Now());
 
     // result is a pair<iterator, bool>.
