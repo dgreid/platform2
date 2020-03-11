62de9855853aa477542e143c504f97c6552fd7ad
C Shapiro <shapiroc@chromium.org>
2020-03-11 11:13:01 -0500

    chromeos-config: Proto coverter command
    Registering the proto converter command that will be used to apply the
    transform via chromeos-config ebuild.
    
    BUG=None
    TEST=None
    
    Change-Id: I8bc12ed170f6c139cfbc6fc44156f12fda283c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2098329
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Commit-Queue: C Shapiro <shapiroc@chromium.org>

--- chromeos-config/setup.py
+++ chromeos-config/setup.py
@@ -22,6 +22,7 @@ setup(
     entry_points={
         'console_scripts': [
             'cros_config_host = cros_config_host.cros_config_host:main',
+            'cros_config_proto_converter = cros_config_host.cros_config_proto_converter:main',
             'cros_config_schema = cros_config_host.cros_config_schema:main',
             'cros_config_test_schema = \
                 cros_config_host.cros_config_test_schema:main',
