b60e93a8896f6d0cd12c9158c8041ed04ad16977
Fergus Dall <sidereal@google.com>
2020-03-27 14:04:23 +1100

    vm_tools: Allow setting metrics consent for testing
    The crash reporter tests use a mock consent file which causes crash
    reporter to ignore the actual metrics consent state. But because the
    mock file is not accessible from the VM, the in-VM crash_reporter will
    not produce crash reports. Make cicerone check for this when telling
    the VM whether or not metrics consent has been granted on test images.
    
    BUG=chromium:703926
    TEST=tast run $IP_ADDR crostini.CrashReporter.download
    
    Change-Id: I3d44650def3cb776620a12c6e42888838c7b32a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2123699
    Tested-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Commit-Queue: Fergus Dall <sidereal@google.com>

--- vm_tools/cicerone/crash_listener_impl.cc
+++ vm_tools/cicerone/crash_listener_impl.cc
@@ -7,16 +7,52 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <base/files/file_util.h>
 #include <base/files/scoped_file.h>
 #include <base/optional.h>
 #include <base/posix/eintr_wrapper.h>
+#include <base/strings/string_util.h>
 #include <base/threading/thread_task_runner_handle.h>
+#include <brillo/key_value_store.h>
 #include <brillo/process.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <google/protobuf/text_format.h>
 
 #include "vm_tools/cicerone/service.h"
 #include "vm_tools/cicerone/virtual_machine.h"
+
+namespace {
+
+// In testing, crash_reporter uses a mock consent system to avoid flake in the
+// real metrics consent. The in-VM crash_reporter can't check this itself for
+// the same reason it can't check the actual metrics consent state, so we need
+// to take this into account in our RPC. This is controlled by the existence of
+// a file at a known location, and should only be checked on test images.
+bool CheckForMockCrashReporterConsent() {
+  brillo::KeyValueStore store;
+  std::string channel;
+  if (!store.Load(base::FilePath("/etc/lsb-release"))) {
+    // Return false here to ensure the expectations are updated if
+    // /etc/lsb-release changes
+    LOG(WARNING) << "Failed to parse /etc/lsb-release, assuming non-test image";
+    return false;
+  }
+
+  if (!store.GetString("CHROMEOS_RELEASE_TRACK", &channel)) {
+    LOG(WARNING) << "Couldn't find release track an /etc/lsb-release, assuming "
+                    "non-test image";
+    return false;
+  }
+
+  if (!base::StartsWith(channel, "test", base::CompareCase::SENSITIVE)) {
+    // Not a test image, mock consent should be disregarded.
+    return false;
+  }
+
+  return base::PathExists(base::FilePath("/run/crash_reporter/mock-consent"));
+}
+
+}  // namespace
 
 namespace vm_tools {
 namespace cicerone {
@@ -29,7 +65,8 @@ grpc::Status CrashListenerImpl::CheckMetricsConsent(
     grpc::ServerContext* ctx,
     const EmptyMessage* request,
     MetricsConsentResponse* response) {
-  response->set_consent_granted(metrics_.AreMetricsEnabled());
+  response->set_consent_granted(metrics_.AreMetricsEnabled() ||
+                                CheckForMockCrashReporterConsent());
   return grpc::Status::OK;
 }
 
