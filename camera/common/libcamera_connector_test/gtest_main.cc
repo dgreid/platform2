d90b19228b5aebf080541e3741fc343d35a93d01
Shik Chen <shik@chromium.org>
2020-04-10 17:12:43 +0800

    camera: libcamera_connector_test: Add GetInfo test
    BUG=b:151047930
    TEST=Pass cros_camera_connector_test on nocturne.
    
    Change-Id: I476d3f3f0a147300535e5b28008293556fcf89b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2145514
    Commit-Queue: Shik Chen <shik@chromium.org>
    Tested-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>

--- camera/common/libcamera_connector_test/gtest_main.cc
+++ camera/common/libcamera_connector_test/gtest_main.cc
@@ -4,7 +4,10 @@
  * found in the LICENSE file.
  */
 
+#include <vector>
+
 #include <base/command_line.h>
+#include <base/strings/stringprintf.h>
 #include <brillo/syslog_logging.h>
 #include <gtest/gtest.h>
 
@@ -13,6 +16,40 @@
 
 namespace cros {
 namespace tests {
+
+namespace {
+
+std::string FourccToString(uint32_t fourcc) {
+  std::string result = "0000";
+  for (int i = 0; i < 4; i++) {
+    auto c = static_cast<char>(fourcc & 0xFF);
+    if (c <= 0x1f || c >= 0x7f) {
+      return base::StringPrintf("%#x", fourcc);
+    }
+    result[i] = c;
+    fourcc >>= 8;
+  }
+  return result;
+}
+
+std::string CameraFormatInfoToString(const cros_cam_format_info_t& info) {
+  return base::StringPrintf("%s %4ux%4u %3ufps",
+                            FourccToString(info.fourcc).c_str(), info.width,
+                            info.height, info.fps);
+}
+
+void DumpCameraInfo(const cros_cam_info_t& info) {
+  LOGF(INFO) << "id: " << info.id;
+  LOGF(INFO) << "name: " << info.name;
+  LOGF(INFO) << "format_count: " << info.format_count;
+  for (int i = 0; i < info.format_count; i++) {
+    LOGF(INFO) << base::StringPrintf(
+        "Format %2d: %s", i,
+        CameraFormatInfoToString(info.format_info[i]).c_str());
+  }
+}
+
+}  // namespace
 
 class ConnectorEnvironment : public ::testing::Environment {
  public:
@@ -27,7 +64,49 @@ class ConnectorEnvironment : public ::testing::Environment {
   }
 };
 
+class CameraClient {
+ public:
+  size_t GetCameraCount() { return camera_infos_.size(); }
+
+  int GetCameraInfo() {
+    int ret = cros_cam_get_cam_info(&CameraClient::GetCamInfoCallback, this);
+    // All connected cameras should be already reported by the callback
+    // function, set the frozen flag to capture unexpected hotplug events
+    // during test. Please see the comment of cros_cam_get_cam_info() for more
+    // details.
+    camera_info_frozen_ = true;
+    return ret;
+  }
+
+ private:
+  int GotCameraInfo(const cros_cam_info_t* info, unsigned is_removed) {
+    EXPECT_FALSE(camera_info_frozen_) << "unexpected hotplug events";
+    EXPECT_EQ(is_removed, 0) << "unexpected removing events";
+    EXPECT_GT(info->format_count, 0) << "no available formats";
+    camera_infos_.push_back(*info);
+    LOGF(INFO) << "Got camera info";
+    DumpCameraInfo(*info);
+    return 0;
+  }
+
+  static int GetCamInfoCallback(void* context,
+                                const cros_cam_info_t* info,
+                                unsigned is_removed) {
+    auto* self = reinterpret_cast<CameraClient*>(context);
+    return self->GotCameraInfo(info, is_removed);
+  }
+
+  std::vector<cros_cam_info_t> camera_infos_;
+  bool camera_info_frozen_ = false;
+};
+
 TEST(ConnectorTest, GetInfo) {
+  CameraClient client;
+  ASSERT_EQ(client.GetCameraInfo(), 0);
+  EXPECT_GT(client.GetCameraCount(), 0) << "no camera found";
+}
+
+TEST(ConnectorTest, Capture) {
   // TODO(b/151047930): Implement the test.
 }
 
@@ -36,7 +115,7 @@ TEST(ConnectorTest, GetInfo) {
 
 int main(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
-  brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderrIfTty);
+  brillo::InitLog(brillo::kLogToStderr);
 
   ::testing::AddGlobalTestEnvironment(new cros::tests::ConnectorEnvironment());
   ::testing::InitGoogleTest(&argc, argv);
