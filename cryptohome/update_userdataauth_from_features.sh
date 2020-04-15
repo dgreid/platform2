b1c5df5330d8eff31da0cb245371793c22ccbbf5
John L Chen <zuan@chromium.org>
2020-04-16 06:23:07 +0800

    cryptohome: Add script and service to update userdataauth flag.
    This change add the script and service that would query chrome features
    service and set the flag that determine if UserDataAuth is enabled or not.
    
    BUG=b:151062600
    TEST=Deploy, reboot, and check flag file and logs.
    
    Cq-Depend: chromium:2151765
    Change-Id: I6e0ef9c9421055eb1aa543184c7559ed3f7075ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2151486
    Tested-by: John L Chen <zuan@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Commit-Queue: John L Chen <zuan@chromium.org>

---
+++ cryptohome/update_userdataauth_from_features.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+try_once() {
+  local ctrl_file="/var/lib/cryptohome/cryptohome_userdataauth_interface.conf"
+  local dbus_interface="org.chromium.ChromeFeaturesServiceInterface"
+  local dbus_method="IsCryptohomeUserDataAuthEnabled"
+  mkdir -p "$(dirname "${ctrl_file}")"
+
+  local status=0
+  local reply
+  reply="$(minijail0 -u chronos /usr/bin/dbus-send --system \
+    --type=method_call --print-reply \
+    --dest=org.chromium.ChromeFeaturesService \
+    /org/chromium/ChromeFeaturesService \
+    "${dbus_interface}.${dbus_method}" \
+    2>/dev/null)" || status=$?
+  if [ "${status}" -ne 0 ]; then
+    # The command failed.
+    logger -p WARN "Failed to contact chrome features service to" \
+      "check if cryptohome UserDataAuth interface is enabled;" \
+      "status=${status} reply=${reply}"
+    return 1
+  fi
+
+  if [ "${reply##* }" = "false" ] ; then
+    # Disable cryptohome userdataauth.
+    echo "USER_DATA_AUTH_INTERFACE=off" > "${ctrl_file}"
+    logger -p INFO "Disabling cryptohome UserDataAuth due to chrome" \
+      "features"
+  elif [ "${reply##* }" = "true" ] ; then
+    # Enable cryptohome userdataauth.
+    echo "USER_DATA_AUTH_INTERFACE=on" > "${ctrl_file}"
+    logger -p INFO "Enabling cryptohome UserDataAuth due to chrome" \
+      "features"
+  else
+    # Response is bad.
+    logger -p WARN "Bad response from chrome features service when" \
+      "checking if cryptohome UserDataAuth interface is enabled;" \
+      "reply=${reply}"
+    return 1
+  fi
+
+  return 0
+}
+
+if [ "$1" = "--once" ]; then
+  try_once
+  exit "$?"
+fi
+
+# If not --once, then we'll try multiple times with exponential backoff.
+for delay in 0.5 1.0 2.0 4.0 8.0 16.0; do
+  if try_once; then
+    # Success, we are done.
+    exit 0
+  fi
+  sleep "${delay}"
+done
