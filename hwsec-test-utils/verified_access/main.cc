6bd73255679114f74ecf971677e2ccb71fc5fd3e
Leo Lai <cylai@google.com>
2020-04-29 11:04:33 +0800

    hwsec-test-utils: VA challenge generation exposed via executable
    This CL enables the usage of the DUT-generated VA challenge for testing.
    
    BUG=b:154672618
    TEST=build and unittests.
    TEST=attestationd can parse the generated challenge with the va signing
    key altered.
    
    Change-Id: I0026b1a9628f8d79f432cbf1c0c4c90ff4b3b00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2203881
    Tested-by: Leo Lai <cylai@google.com>
    Commit-Queue: Leo Lai <cylai@google.com>
    Reviewed-by: Louis Collard <louiscollard@chromium.org>

--- hwsec-test-utils/verified_access/main.cc
+++ hwsec-test-utils/verified_access/main.cc
@@ -4,11 +4,60 @@
 
 #include "hwsec-test-utils/verified_access/verified_access.h"
 
+#include <stdio.h>
+
+#include <base/base64.h>
 #include <base/command_line.h>
+#include <base/optional.h>
+#include <brillo/data_encoding.h>
 #include <brillo/syslog_logging.h>
+
+namespace {
+
+constexpr char kGenerateCommand[] = "generate";
+const char kUsage[] = R"(
+Usage: hwsec-test-va <command> [<args>]
+Commands:
+  |generate|
+      Generates a VA challenge signed with well-known VA signing key and prints
+      the base64-encoded result in stdout.
+)";
+
+constexpr char kExpectedChallengePrefix[] = "EnterpriseKeyChallenge";
+
+void PrintUsage() {
+  printf("%s", kUsage);
+}
+
+}  // namespace
 
 int main(int argc, char* argv[]) {
   base::CommandLine::Init(argc, argv);
   brillo::InitLog(brillo::kLogToStderr);
-  return 0;
+
+  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+  const auto& args = cl->GetArgs();
+  if (args.empty()) {
+    PrintUsage();
+    return 1;
+  }
+  if (args.front() == kGenerateCommand) {
+    hwsec_test_utils::verified_access::VerifiedAccessChallenge va;
+    base::Optional<attestation::SignedData> challenge =
+        va.GenerateChallenge(kExpectedChallengePrefix);
+    if (!challenge) {
+      printf("Failed to generate VA challenge.\n");
+      return 1;
+    }
+    std::string serialized_challenge;
+    if (!challenge->SerializeToString(&serialized_challenge)) {
+      printf("Failed to serialize VA challenge.\n");
+      return 1;
+    }
+    printf("%s",
+           brillo::data_encoding::Base64Encode(serialized_challenge).c_str());
+    return 0;
+  }
+  PrintUsage();
+  return 1;
 }
