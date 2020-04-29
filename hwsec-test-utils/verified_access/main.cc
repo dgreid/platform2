216261778fa77d4790f391f7f4f28cb7c84ca0c7
Leo Lai <cylai@google.com>
2020-04-29 11:04:33 +0800

    hwsec-test-utils: VA challenge verification exposed via executable
    This CL enables the usage of the verification of challenge response for
    testing.
    
    BUG=b:154672618
    TEST=unittests.
    TEST=Verified a challenge response from a readl DUT, w/ or w/o --binary.
    
    Change-Id: Ic6485b95de2539b970289e9cf16795790ad7fccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2211629
    Tested-by: Leo Lai <cylai@google.com>
    Commit-Queue: Leo Lai <cylai@google.com>
    Reviewed-by: Louis Collard <louiscollard@chromium.org>

--- hwsec-test-utils/verified_access/main.cc
+++ hwsec-test-utils/verified_access/main.cc
@@ -5,9 +5,12 @@
 #include "hwsec-test-utils/verified_access/verified_access.h"
 
 #include <stdio.h>
+#include <utility>
 
 #include <base/base64.h>
 #include <base/command_line.h>
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
 #include <base/optional.h>
 #include <brillo/data_encoding.h>
 #include <brillo/syslog_logging.h>
@@ -15,12 +18,21 @@
 namespace {
 
 constexpr char kGenerateCommand[] = "generate";
+constexpr char kVerifyCommand[] = "verify";
+
+constexpr char kInputSwitch[] = "input";
+constexpr char kBinarySwitch[] = "binary";
+
 const char kUsage[] = R"(
 Usage: hwsec-test-va <command> [<args>]
 Commands:
   |generate|
       Generates a VA challenge signed with well-known VA signing key and prints
       the base64-encoded result in stdout.
+  |verify| --input=<filename> [--binary]
+      Verifies the VA challenge response from attestation service. By default,
+      reads last argument that follows as the base64-encoded response. If the
+      input is in binary (i.e., not base64-encoded), specify '--binary'.
 )";
 
 constexpr char kExpectedChallengePrefix[] = "EnterpriseKeyChallenge";
@@ -41,6 +53,7 @@ int main(int argc, char* argv[]) {
     PrintUsage();
     return 1;
   }
+
   if (args.front() == kGenerateCommand) {
     hwsec_test_utils::verified_access::VerifiedAccessChallenge va;
     base::Optional<attestation::SignedData> challenge =
@@ -58,6 +71,45 @@ int main(int argc, char* argv[]) {
            brillo::data_encoding::Base64Encode(serialized_challenge).c_str());
     return 0;
   }
+
+  if (args.front() == kVerifyCommand) {
+    const std::string path = cl->GetSwitchValueASCII(kInputSwitch);
+    if (path.empty()) {
+      printf("No valid file path specified.\n");
+      return 1;
+    }
+    std::string data;
+    if (!base::ReadFileToString(base::FilePath(path), &data)) {
+      printf("Failed to read file.\n");
+      return 1;
+    }
+    if (!cl->HasSwitch(kBinarySwitch)) {
+      std::string tmp;
+      if (!brillo::data_encoding::Base64Decode(data, &tmp)) {
+        printf("Failed to base64 decode the file content.\n");
+        return 1;
+      }
+      data = std::move(tmp);
+    }
+    // At this point, |data| is the binary representation regardless of the
+    // given data format.
+    attestation::SignedData signed_challenge_response;
+    if (!signed_challenge_response.ParseFromString(data)) {
+      printf("Failed to parse serialized |attestation::SignedData|.\n");
+      return 1;
+    }
+
+    hwsec_test_utils::verified_access::VerifiedAccessChallenge va;
+    if (!va.VerifyChallengeResponse(signed_challenge_response,
+                                    kExpectedChallengePrefix)) {
+      printf("Failed to verify challenge response.\n");
+      return 1;
+    }
+    printf("Succeeded.\n");
+    return 0;
+  }
+
+  // None of the command matches; print usage and return non-zero exit status.
   PrintUsage();
   return 1;
 }
