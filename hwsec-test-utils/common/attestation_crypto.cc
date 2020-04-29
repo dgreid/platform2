eceb4d2000ff2a6f3983ea3073f7e1b350a3fac9
Leo Lai <cylai@google.com>
2020-04-29 11:04:33 +0800

    hwsec-test-utils: |Decrypt| for |attestation::EncryptedData|.
    This Cl implements a compound operations that decrypt the data in
    |attestation::EncryptedData|.
    
    The first consumer code will be
    |VerifiedAccessChallenge::VerifyChallengeResponse| in the follow-up CL,
    where we have to decrypt |KeyInfo|.
    
    The |KeyDeriver| is used to dynamically inject the key derivation
    function. Though in the hwsec-test-utils we don't expect the usecase of
    other derivation in decryption case, considering portability potential
    usage in unittests we reserve the feature of |KeyDeriver| as the
    function input.
    
    BUG=b:154672618
    TEST=build ok.
    TEST=work with the consumer code in the follow up CL
    
    Change-Id: I234bf54336ddbc5572674a17a9d7990a9aa87ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2211627
    Tested-by: Leo Lai <cylai@google.com>
    Commit-Queue: Leo Lai <cylai@google.com>
    Reviewed-by: Louis Collard <louiscollard@chromium.org>

---
+++ hwsec-test-utils/common/attestation_crypto.cc
@@ -0,0 +1,71 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "hwsec-test-utils/common/attestation_crypto.h"
+
+#include <string>
+
+#include <base/optional.h>
+
+#include "hwsec-test-utils/common/openssl_utility.h"
+
+namespace hwsec_test_utils {
+namespace attestation_crypto {
+
+std::string KeyDeriverDirect::ToAesKey(const std::string& seed) const {
+  return seed;
+}
+
+std::string KeyDeriverDirect::ToHmacKey(const std::string& seed) const {
+  return seed;
+}
+
+ReturnStatus Decrypt(const attestation::EncryptedData& encrypted_data_proto,
+                     const crypto::ScopedEVP_PKEY& key,
+                     const KeyDeriverBase& key_deriver,
+                     std::string* decrypted) {
+  // Decrypt.
+  base::Optional<std::string> seed = EVPRsaDecrypt(
+      key, encrypted_data_proto.wrapped_key(), RSA_PKCS1_OAEP_PADDING);
+  if (!seed) {
+    return ReturnStatus::kUnwrapKey;
+  }
+  const std::string aes_key = key_deriver.ToAesKey(*seed);
+  base::Optional<std::string> decrypted_data =
+      EVPAesDecrypt(encrypted_data_proto.encrypted_data(), EVP_aes_256_cbc(),
+                    aes_key, encrypted_data_proto.iv());
+  if (!decrypted_data) {
+    LOG(ERROR) << __func__ << ": Failed to decrypt data.";
+    return ReturnStatus::kDecrypt;
+  }
+
+  // Verify HMAC.
+  const std::string hmac_key_str = key_deriver.ToHmacKey(*seed);
+  crypto::ScopedEVP_PKEY hmac_key(EVP_PKEY_new_mac_key(
+      EVP_PKEY_HMAC, nullptr,
+      reinterpret_cast<const unsigned char*>(hmac_key_str.data()),
+      hmac_key_str.length()));
+  if (!hmac_key) {
+    LOG(ERROR) << __func__ << ": Failed to call EVP_PKEY_new_mac_key: "
+               << GetOpenSSLError();
+    return ReturnStatus::kFailure;
+  }
+  base::Optional<std::string> hmac = EVPDigestSign(
+      hmac_key, EVP_sha512(),
+      encrypted_data_proto.iv() + encrypted_data_proto.encrypted_data());
+  if (!hmac) {
+    LOG(ERROR) << __func__ << ": Failed to calculate HMAC.";
+    return ReturnStatus::kFailure;
+  }
+  if (*hmac != encrypted_data_proto.mac()) {
+    LOG(ERROR) << __func__ << ": HMAC mismatch.";
+    return ReturnStatus::kHmac;
+  }
+  *decrypted = *decrypted_data;
+  return ReturnStatus::kSuccess;
+}
+
+}  // namespace attestation_crypto
+
+}  // namespace hwsec_test_utils
