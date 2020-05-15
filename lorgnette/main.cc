92289334912316c675c0f025bc211a1715d862cd
Benjamin Gordon <bmgordon@chromium.org>
2020-05-15 13:28:53 -0600

    lorgnette: Tighten minijail setup
    Previously, lorgnette was launched directly from dbus as root.  It
    sandboxed itself with these steps:
    
    1. Fork into the background.
    2. Claim its dbus name.
    3. Launch a logger process running as syslog.
    4. Enter a separate VFS namespace.
    5. Drop privileges to the saned user.
    
    Now that dbus indirectly launches lorgnette through upstart, we can
    launch lorgnette through minijail0 instead of having it internally set
    up the sandbox.  This simplifies the setup and results in a much tighter
    sandbox.
    
    Even though lorgnette no longer launches child processes for scanning or
    pnm2png conversion, it still needs to launch a logger subprocess to
    capture anything libsane writes to stderr.  Instead of relying on the
    --foreground flag to decide if this should happen, check if stdin is a
    tty.  This captures the output to syslog when run normally while still
    leaving it visible on stderr if run manually for testing.
    
    Overall, this results in these changes:
    
    1. Don't fork into the background.  minijail0 calls fork to start the
       process and upstart still keeps track of the child.
    2. Launch directly as saned.
       2a. logger gets run as the saned user instead of syslog.
       2b. dbus allows saned to claim org.chromium.lorgnette in addition to
           root.
    3. Much tighter VFS sandbox and add other features provided by
       minijail (nnp, more namespaces, etc).
    
    BUG=b:153152860
    TEST=Scanned a page from several scanners
    
    Change-Id: Ib2528803fac18af120db7a6ec1cc29d0172c85f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2204662
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Fletcher Woodruff <fletcherw@chromium.org>
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>
    Tested-by: Benjamin Gordon <bmgordon@chromium.org>

--- lorgnette/main.cc
+++ lorgnette/main.cc
@@ -2,132 +2,59 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <errno.h>
 #include <stdio.h>
-#include <time.h>
 #include <unistd.h>
 
-#include <string>
-#include <vector>
-
 #include <base/bind.h>
-#include <base/command_line.h>
 #include <base/logging.h>
-#include <brillo/minijail/minijail.h>
+#include <brillo/process.h>
 #include <brillo/syslog_logging.h>
 
 #include "lorgnette/daemon.h"
 
-using std::string;
-using std::vector;
-
-namespace switches {
-
-// Don't daemon()ize; run in foreground.
-static const char kForeground[] = "foreground";
-// Flag that causes lorgnette to show the help message and exit.
-static const char kHelp[] = "help";
-
-// The help message shown if help flag is passed to the program.
-static const char kHelpMessage[] =
-    "\n"
-    "Available Switches: \n"
-    "  --foreground\n"
-    "    Don\'t daemon()ize; run in foreground.\n";
-}  // namespace switches
-
 namespace {
 
 const char* kLoggerCommand = "/usr/bin/logger";
-const char* kLoggerUser = "syslog";
 
 }  // namespace
 
 // Always logs to the syslog and logs to stderr if
-// we are running in the foreground.
-void SetupLogging(brillo::Minijail* minijail,
-                  bool foreground,
-                  const char* daemon_name) {
-  int log_flags = 0;
-  log_flags |= brillo::kLogToSyslog;
-  log_flags |= brillo::kLogHeader;
-  if (foreground) {
-    log_flags |= brillo::kLogToStderr;
-  }
-  brillo::InitLog(log_flags);
+// we are connected to a tty.
+void SetupLogging(const char* daemon_name) {
+  brillo::InitLog(
+      brillo::kLogToSyslog | brillo::kLogToStderrIfTty | brillo::kLogHeader);
 
-  if (!foreground) {
-    vector<char*> logger_command_line;
-    int logger_stdin_fd;
-    logger_command_line.push_back(const_cast<char*>(kLoggerCommand));
-    logger_command_line.push_back(const_cast<char*>("--priority"));
-    logger_command_line.push_back(const_cast<char*>("daemon.err"));
-    logger_command_line.push_back(const_cast<char*>("--tag"));
-    logger_command_line.push_back(const_cast<char*>(daemon_name));
-    logger_command_line.push_back(nullptr);
+  if (!isatty(STDIN_FILENO)) {
+    brillo::ProcessImpl logger;
+    logger.AddArg(kLoggerCommand);
+    logger.AddArg("--priority");
+    logger.AddArg("daemon.err");
+    logger.AddArg("--tag");
+    logger.AddArg(daemon_name);
 
-    struct minijail* jail = minijail->New();
-    minijail->DropRoot(jail, kLoggerUser, kLoggerUser);
-
-    if (!minijail->RunPipeAndDestroy(jail, logger_command_line, nullptr,
-                                     &logger_stdin_fd)) {
-      LOG(ERROR) << "Unable to spawn logger. "
-                 << "Writes to stderr will be discarded.";
+    logger.RedirectUsingPipe(STDIN_FILENO, true);
+    if (!logger.Start()) {
+      LOG(ERROR) << "Failed to start logger child.";
       return;
     }
 
     // Note that we don't set O_CLOEXEC here. This means that stderr
     // from any child processes will, by default, be logged to syslog.
-    if (dup2(logger_stdin_fd, fileno(stderr)) != fileno(stderr)) {
+    if (dup2(logger.GetPipe(STDIN_FILENO), fileno(stderr)) != fileno(stderr)) {
       PLOG(ERROR) << "Failed to redirect stderr to syslog";
+      return;
     }
-    close(logger_stdin_fd);
+
+    logger.Release();
   }
 }
 
-// Enter a sanboxed vfs namespace.
-void EnterVFSNamespace(brillo::Minijail* minijail) {
-  struct minijail* jail = minijail->New();
-  minijail_namespace_vfs(jail);
-  minijail_enter(jail);
-  minijail->Destroy(jail);
-}
-
-void DropPrivileges(brillo::Minijail* minijail) {
-  struct minijail* jail = minijail->New();
-  minijail_inherit_usergroups(jail);  // Keep access to ippusb group.
-  minijail->DropRoot(jail, lorgnette::Daemon::kScanUserName,
-                     lorgnette::Daemon::kScanGroupName);
-  minijail_enter(jail);
-  minijail->Destroy(jail);
-}
-
-void OnStartup(const char* daemon_name, base::CommandLine* cl) {
-  brillo::Minijail* minijail = brillo::Minijail::GetInstance();
-  SetupLogging(minijail, cl->HasSwitch(switches::kForeground), daemon_name);
-
-  LOG(INFO) << __func__ << ": Dropping privileges";
-  EnterVFSNamespace(minijail);
-
-  // Now that the daemon has all the resources it needs to run, we can drop
-  // privileges further.
-  DropPrivileges(minijail);
+void OnStartup(const char* daemon_name) {
+  SetupLogging(daemon_name);
 }
 
 int main(int argc, char** argv) {
-  base::CommandLine::Init(argc, argv);
-  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
-
-  if (cl->HasSwitch(switches::kHelp)) {
-    LOG(INFO) << switches::kHelpMessage;
-    return 0;
-  }
-
-  const int nochdir = 0, noclose = 0;
-  if (!cl->HasSwitch(switches::kForeground))
-    PLOG_IF(FATAL, daemon(nochdir, noclose) == -1) << "Failed to daemonize";
-
-  lorgnette::Daemon daemon(base::Bind(&OnStartup, argv[0], cl));
+  lorgnette::Daemon daemon(base::Bind(&OnStartup, argv[0]));
 
   daemon.Run();
 
