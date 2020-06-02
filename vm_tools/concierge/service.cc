dc22ed5a1185cf491eae2f0edd93d9a765171157
Dmitry Torokhov <dtor@chromium.org>
2020-06-02 09:53:53 -0700

    vm_tools: concierge: only complain about missing paths when creating disks
    GetDiskPathFromName() is called when trying to determine if a VM exists,
    thus we should not log errors when directories whether either crosvms or
    Plugin VMs are supposed to be located do not exist.
    
    BUG=None
    TEST=tast run -buildbundle=pita <IP> pita.Reboot and check that there
    are no messages like "ERR vm_concierge[9266]: Failed to get qcow2 path"
    
    Change-Id: Ica15558a95567d7a3c3ebb19a716440abce7e39b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2227291
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Commit-Queue: Dmitry Torokhov <dtor@chromium.org>
    Tested-by: Dmitry Torokhov <dtor@chromium.org>

--- vm_tools/concierge/service.cc
+++ vm_tools/concierge/service.cc
@@ -352,14 +352,16 @@ bool GetDiskPathFromName(
           GetFilePathFromName(cryptohome_id, vm_name, storage_location,
                               kQcowImageExtension, create_parent_dir);
       if (!qcow2_path) {
-        LOG(ERROR) << "Failed to get qcow2 path";
+        if (create_parent_dir)
+          LOG(ERROR) << "Failed to get qcow2 path";
         return false;
       }
       const auto raw_path =
           GetFilePathFromName(cryptohome_id, vm_name, storage_location,
                               kRawImageExtension, create_parent_dir);
       if (!raw_path) {
-        LOG(ERROR) << "Failed to get raw path";
+        if (create_parent_dir)
+          LOG(ERROR) << "Failed to get raw path";
         return false;
       }
 
@@ -399,7 +401,8 @@ bool GetDiskPathFromName(
           GetFilePathFromName(cryptohome_id, vm_name, storage_location,
                               kPluginVmImageExtension, create_parent_dir);
       if (!plugin_path) {
-        LOG(ERROR) << "failed to get plugin path";
+        if (create_parent_dir)
+          LOG(ERROR) << "failed to get plugin path";
         return false;
       }
       *path_out = *plugin_path;
