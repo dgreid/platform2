46b640dfaa12752cc31e830ccb5a3b08f27bd6e8
Xiaoyong Zhou <xzhou@google.com>
2020-06-22 16:51:53 -0700

    cryptohome: add code to parse fido messages
    This CL parses FIDO MakeCredentialResponse and extracts
    the newly created credential public key and HMAC extensions.
    This CL is partially ported from <chromium>/src/device/fido/.
    Because fido protocol does not change often and chromium is built
    on Boring SSL, we decided not to share the code.
    
    BUG=b:143770026
    TEST=FEATURES=test emerge-nami system_api cryptohome
    
    Change-Id: I3a60d8597ca8f596b647dbdd695b8236d5de24dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2261735
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Tested-by: Xiaoyong Zhou <xzhou@chromium.org>
    Commit-Queue: Xiaoyong Zhou <xzhou@chromium.org>

---
+++ cryptohome/fido/attested_credential_data.h
@@ -0,0 +1,91 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CRYPTOHOME_FIDO_ATTESTED_CREDENTIAL_DATA_H_
+#define CRYPTOHOME_FIDO_ATTESTED_CREDENTIAL_DATA_H_
+
+#include <memory>
+#include <stddef.h>
+#include <stdint.h>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "base/component_export.h"
+#include "base/containers/span.h"
+#include "base/macros.h"
+#include "base/optional.h"
+#include "cryptohome/fido/fido_constants.h"
+
+namespace cryptohome {
+namespace fido_device {
+
+class PublicKey;
+
+// https://www.w3.org/TR/2017/WD-webauthn-20170505/#sec-attestation-data
+class AttestedCredentialData {
+ public:
+  // Parses an |AttestedCredentialData| from a prefix of |*buffer|. Returns
+  // nullopt on error, or else the parse return and a (possibly empty) suffix of
+  // |buffer| that was not parsed.
+  static base::Optional<
+      std::pair<AttestedCredentialData, base::span<const uint8_t>>>
+  ConsumeFromCtapResponse(base::span<const uint8_t> buffer);
+
+  static base::Optional<AttestedCredentialData> CreateFromU2fRegisterResponse(
+      base::span<const uint8_t> u2f_data,
+      std::unique_ptr<PublicKey> public_key);
+
+  // Moveable.
+  AttestedCredentialData(AttestedCredentialData&& other);
+  AttestedCredentialData& operator=(AttestedCredentialData&& other);
+
+  ~AttestedCredentialData();
+
+  const std::vector<uint8_t>& credential_id() const { return credential_id_; }
+
+  // Returns true iff the AAGUID is all zero bytes.
+  bool IsAaguidZero() const;
+
+  const PublicKey* GetPublicKey() const;
+
+  // Invoked when sending "none" attestation statement to the relying party.
+  // Replaces AAGUID with zero bytes.
+  void DeleteAaguid();
+
+  // Produces a byte array consisting of:
+  // * AAGUID (16 bytes)
+  // * Len (2 bytes)
+  // * Credential Id (Len bytes)
+  // * Credential Public Key.
+  std::vector<uint8_t> SerializeAsBytes() const;
+
+  AttestedCredentialData(
+      base::span<const uint8_t, kAaguidLength> aaguid,
+      base::span<const uint8_t, kCredentialIdLengthLength> credential_id_length,
+      std::vector<uint8_t> credential_id,
+      std::unique_ptr<PublicKey> public_key);
+
+  uint16_t GetCredentialIdLength();
+
+  std::string ToString();
+
+ private:
+  // The 16-byte AAGUID of the authenticator.
+  std::array<uint8_t, kAaguidLength> aaguid_;
+
+  // Big-endian length of the credential (i.e. key handle).
+  std::array<uint8_t, kCredentialIdLengthLength> credential_id_length_;
+
+  std::vector<uint8_t> credential_id_;
+
+  std::unique_ptr<PublicKey> public_key_;
+
+  DISALLOW_COPY_AND_ASSIGN(AttestedCredentialData);
+};
+
+}  // namespace fido_device
+}  // namespace cryptohome
+
+#endif  // CRYPTOHOME_FIDO_ATTESTED_CREDENTIAL_DATA_H_
---
+++ cryptohome/fido/public_key.h
@@ -0,0 +1,49 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CRYPTOHOME_FIDO_PUBLIC_KEY_H_
+#define CRYPTOHOME_FIDO_PUBLIC_KEY_H_
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+#include <brillo/secure_blob.h>
+
+#include "base/component_export.h"
+#include "base/macros.h"
+
+namespace cryptohome {
+namespace fido_device {
+
+// https://www.w3.org/TR/2017/WD-webauthn-20170505/#sec-attestation-data.
+class PublicKey {
+ public:
+  PublicKey() = default;
+  // public key
+  virtual ~PublicKey() = default;
+
+  // The credential public key as a COSE_Key map as defined in Section 7
+  // of https://tools.ietf.org/html/rfc8152.
+  virtual std::vector<uint8_t> EncodeAsCOSEKey() const = 0;
+
+  // Convert the public key to DER format for TPM.
+  virtual bool DumpToDer(brillo::SecureBlob* der);
+
+  virtual std::string GetAlgorithmName();
+
+  virtual std::string ToString();
+
+ protected:
+  explicit PublicKey(std::string algorithm);
+
+  std::string algorithm_;
+
+  DISALLOW_COPY_AND_ASSIGN(PublicKey);
+};
+
+}  // namespace fido_device
+}  // namespace cryptohome
+
+#endif  // CRYPTOHOME_FIDO_PUBLIC_KEY_H_
