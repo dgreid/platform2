a171200b8fe9fb7839fb9dd77989862e027f578e
Leo Lai <cylai@google.com>
2020-06-29 13:06:34 +0800

    cryptohome: Unify implementation of tpm-manager
    Since TPM1.2 also uses distributed model, it doesn't make sense to make
    the implementation tpm-version-dependent.
    
    Note that the removal of dump_status is intentional because the only
    consumer, i.e., factory, doesn't care about the change anymore.
    
    BUG=b:160110429
    BUG=b:160207067
    TEST=unittests
    TEST=all commaned of tpm-manager working on chell and soraka
    
    Change-Id: I2fa777ad6dcfd43f2f1a5e815cd42448f16d51e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2275844
    Reviewed-by: Louis Collard <louiscollard@chromium.org>
    Tested-by: Leo Lai <cylai@google.com>

--- cryptohome/tpm_manager.cc
+++ cryptohome/tpm_manager.cc
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <string>
 
+#include <attestation/common/print_interface_proto.h>
+#include <attestation/proto_bindings/interface.pb.h>
 #include <base/command_line.h>
 #include <base/files/file_path.h>
 #include <base/logging.h>
@@ -15,8 +17,14 @@
 #include <brillo/secure_blob.h>
 #include <brillo/syslog_logging.h>
 #include <openssl/evp.h>
+#include <tpm_manager/common/print_tpm_manager_proto.h>
+#include <tpm_manager/proto_bindings/tpm_manager.pb.h>
+
+#include <attestation-client/attestation/dbus-proxies.h>
+#include <tpm_manager-client/tpm_manager/dbus-proxies.h>
 
 #include "cryptohome/cryptohome_metrics.h"
+#include "cryptohome/tpm.h"
 
 namespace cryptohome {
 
@@ -27,27 +35,31 @@ void PrintUsage() {
       base::CommandLine::ForCurrentProcess()->GetProgram().BaseName().value();
   printf("Usage: %s [command] [options]\n", program.c_str());
   printf("  Commands:\n");
-  printf("    initialize: Takes ownership of an unowned TPM and initializes it "
-         "for use with Chrome OS Core. This is the default command.\n"
-         "      - Install attributes will be empty and finalized.\n"
-         "      - Attestation data will be prepared.\n"
-         "      This command may be run safely multiple times and may be "
-         "retried on failure. If the TPM is already initialized this command\n"
-         "      has no effect and exits without error. The --finalize option "
-         "will cause various TPM data to be finalized (this does not affect\n"
-         "      install attributes which are always finalized).\n");
-  printf("    verify_endorsement: Verifies TPM endorsement.\n"
-         "      If the --cros_core option is specified then Chrome OS Core "
-         "endorsement is verified. Otherwise, normal Chromebook endorsement\n"
-         "      is verified. Requires the TPM to be initialized but not "
-         "finalized.\n");
-  printf("    dump_status: Prints TPM status information.\n");
-  printf("    get_random <N>: Gets N random bytes from the TPM and prints them "
-         "as a hex-encoded string.\n");
-  printf("    get_version_info: Prints TPM software and hardware version "
-         "information.\n");
-  printf("    get_ifx_field_upgrade_info: Prints status information pertaining "
-         "to firmware updates on Infineon TPMs.\n");
+  printf(
+      "    initialize: Takes ownership of an unowned TPM and initializes it "
+      "for use with Chrome OS Core. This is the default command.\n"
+      "      - Install attributes will be empty and finalized.\n"
+      "      - Attestation data will be prepared.\n"
+      "      This command may be run safely multiple times and may be "
+      "retried on failure. If the TPM is already initialized this command\n"
+      "      has no effect and exits without error. The --finalize option "
+      "will cause various TPM data to be finalized (this does not affect\n"
+      "      install attributes which are always finalized).\n");
+  printf(
+      "    verify_endorsement: Verifies TPM endorsement.\n"
+      "      If the --cros_core option is specified then Chrome OS Core "
+      "endorsement is verified. Otherwise, normal Chromebook endorsement\n"
+      "      is verified. Requires the TPM to be initialized but not "
+      "finalized.\n");
+  printf(
+      "    get_random <N>: Gets N random bytes from the TPM and prints them "
+      "as a hex-encoded string.\n");
+  printf(
+      "    get_version_info: Prints TPM software and hardware version "
+      "information.\n");
+  printf(
+      "    get_ifx_field_upgrade_info: Prints status information pertaining "
+      "to firmware updates on Infineon TPMs.\n");
   printf("    get_srk_status: Prints SRK status information.\n");
 }
 
@@ -59,19 +71,100 @@ static void PrintIFXFirmwarePackage(
   printf("%s_stale_version %08x\n", prefix, firmware_package.stale_version);
 }
 
-int TakeOwnership(bool finalize);
-int VerifyEK(bool is_cros_core);
-int DumpStatus();
-int GetRandom(unsigned int random_bytes_count);
-bool GetVersionInfo(Tpm::TpmVersionInfo* version_info);
-bool GetIFXFieldUpgradeInfo(Tpm::IFXFieldUpgradeInfo* info);
-bool GetTpmStatus(Tpm::TpmStatusInfo* status);
+int TakeOwnership(bool finalize) {
+  base::Time start_time = base::Time::Now();
+
+  dbus::Bus::Options options;
+  options.bus_type = dbus::Bus::SYSTEM;
+  org::chromium::TpmOwnershipProxy proxy(
+      base::MakeRefCounted<dbus::Bus>(options));
+  brillo::ErrorPtr error;
+
+  LOG(INFO) << "Initializing TPM.";
+  ::tpm_manager::TakeOwnershipRequest request;
+  ::tpm_manager::TakeOwnershipReply reply;
+  if (!proxy.TakeOwnership(request, &reply, &error)) {
+    LOG(ERROR) << "Error sending dbus message to tpm_manager: "
+               << error->GetMessage();
+    return -1;
+  }
+
+  if (reply.status() != ::tpm_manager::STATUS_SUCCESS) {
+    LOG(ERROR) << "Failed to take ownership.";
+    puts(GetProtoDebugString(reply).c_str());
+    return -1;
+  }
+  if (finalize) {
+    LOG(WARNING) << "Finalization is ignored.";
+  }
+  base::TimeDelta duration = base::Time::Now() - start_time;
+  LOG(INFO) << "TPM initialization successful (" << duration.InMilliseconds()
+            << " ms).";
+  return 0;
+}
+
+int VerifyEK(bool is_cros_core) {
+  dbus::Bus::Options options;
+  options.bus_type = dbus::Bus::SYSTEM;
+  org::chromium::AttestationProxy proxy(
+      base::MakeRefCounted<dbus::Bus>(options));
+  brillo::ErrorPtr error;
+
+  attestation::VerifyRequest request;
+  request.set_cros_core(is_cros_core);
+  request.set_ek_only(true);
+  attestation::VerifyReply reply;
+  if (!proxy.Verify(request, &reply, &error)) {
+    LOG(ERROR) << "Error sending dbus message to tpm_manager: "
+               << error->GetMessage();
+    return -1;
+  }
+  if (reply.status() != attestation::AttestationStatus::STATUS_SUCCESS) {
+    LOG(ERROR) << "Failed to verify TPM endorsement.";
+    puts(GetProtoDebugString(reply).c_str());
+    return -1;
+  }
+  if (!reply.verified()) {
+    LOG(ERROR) << "TPM endorsement verification failed.";
+    return -1;
+  }
+  LOG(INFO) << "TPM endorsement verified successfully.";
+  return 0;
+}
+
+int GetRandom(unsigned int random_bytes_count) {
+  cryptohome::Tpm* tpm = cryptohome::Tpm::GetSingleton();
+  brillo::SecureBlob random_bytes;
+  tpm->GetRandomDataSecureBlob(random_bytes_count, &random_bytes);
+  if (random_bytes_count != random_bytes.size())
+    return -1;
+
+  std::string hex_bytes =
+      base::HexEncode(random_bytes.data(), random_bytes.size());
+  puts(hex_bytes.c_str());
+  return 0;
+}
+
+bool GetVersionInfo(cryptohome::Tpm::TpmVersionInfo* version_info) {
+  cryptohome::Tpm* tpm = cryptohome::Tpm::GetSingleton();
+  return tpm->GetVersionInfo(version_info);
+}
+
+bool GetIFXFieldUpgradeInfo(cryptohome::Tpm::IFXFieldUpgradeInfo* info) {
+  cryptohome::Tpm* tpm = cryptohome::Tpm::GetSingleton();
+  return tpm->GetIFXFieldUpgradeInfo(info);
+}
+
+bool GetTpmStatus(cryptohome::Tpm::TpmStatusInfo* status) {
+  cryptohome::Tpm* tpm = cryptohome::Tpm::GetSingleton();
+  tpm->GetStatus(0, status);
+  return true;
+}
 
 }  // namespace tpm_manager
 
 }  // namespace cryptohome
 
-using cryptohome::tpm_manager::DumpStatus;
 using cryptohome::tpm_manager::GetIFXFieldUpgradeInfo;
 using cryptohome::tpm_manager::GetRandom;
 using cryptohome::tpm_manager::GetTpmStatus;
@@ -81,9 +174,9 @@ using cryptohome::tpm_manager::PrintUsage;
 using cryptohome::tpm_manager::TakeOwnership;
 using cryptohome::tpm_manager::VerifyEK;
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
-  base::CommandLine *command_line = base::CommandLine::ForCurrentProcess();
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderr);
   OpenSSL_add_all_algorithms();
   cryptohome::ScopedMetricsInitializer metrics_initializer;
@@ -102,9 +195,6 @@ int main(int argc, char **argv) {
   if (command == "verify_endorsement") {
     return VerifyEK(command_line->HasSwitch("cros_core"));
   }
-  if (command == "dump_status") {
-    return DumpStatus();
-  }
   unsigned int random_bytes_count = 0;
   if (command == "get_random" && arguments.size() == 2 &&
       base::StringToUint(arguments[1], &random_bytes_count) &&
@@ -121,20 +211,21 @@ int main(int argc, char **argv) {
     std::string vendor_specific = base::ToLowerASCII(
         base::HexEncode(version_info.vendor_specific.data(),
                         version_info.vendor_specific.size()));
-    printf("tpm_family %08" PRIx32 "\n"
-           "spec_level %016" PRIx64 "\n"
-           "vendor %08" PRIx32 "\n"
-           "tpm_model %08" PRIx32 "\n"
-           "firmware_version %016" PRIx64 "\n"
+    printf("tpm_family %08" PRIx32
+           "\n"
+           "spec_level %016" PRIx64
+           "\n"
+           "vendor %08" PRIx32
+           "\n"
+           "tpm_model %08" PRIx32
+           "\n"
+           "firmware_version %016" PRIx64
+           "\n"
            "vendor_specific %s\n"
            "version_fingerprint %" PRId32 " %08" PRIx32 "\n",
-           version_info.family,
-           version_info.spec_level,
-           version_info.manufacturer,
-           version_info.tpm_model,
-           version_info.firmware_version,
-           vendor_specific.c_str(),
-           fingerprint,
+           version_info.family, version_info.spec_level,
+           version_info.manufacturer, version_info.tpm_model,
+           version_info.firmware_version, vendor_specific.c_str(), fingerprint,
            fingerprint);
     return 0;
   }
