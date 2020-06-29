17cb86c484a99097db30717f5c425dc80b6c6c59
Tom Hughes <tomhughes@chromium.org>
2020-06-29 11:56:21 -0700

    chromeos-config: Run cros lint as part of the tests
    BUG=chromium:1101555
    TEST=./run_tests.sh
    
    Change-Id: Ib8467d9146e0f7b99d641652453e25eab513ee6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2274073
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Auto-Submit: Tom Hughes <tomhughes@chromium.org>

--- chromeos-config/run_tests.sh
+++ chromeos-config/run_tests.sh
@@ -6,3 +6,10 @@
 # Script to run all Python unit tests in cros_config.
 
 python3 -m unittest discover -p '*test.py'
+
+# Run linter
+# TODO(https://crbug.com/1101555): "cros lint" doesn't work when run as part of
+# an ebuild.
+if which cros; then
+  find . -name '*.py' -exec cros lint --py3 {} +
+fi
