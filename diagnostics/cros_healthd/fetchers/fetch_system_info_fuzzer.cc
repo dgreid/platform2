ae4b4b8b0592b001f8c73ebb7d646196841ac516
Kartik Hegde <khegde@chromium.org>
2020-08-04 23:24:10 -0600

    diagnostics: Fix system info fuzzer
    Edit the fuzzer to write given input data to the files that the system
    info will read from. Add constraints for extremely large input size.
    Previously, the fuzzer, running on the workstation, took ~28000 ms to
    complete on the failing test case (which can be found in the linked
    BUG), likely causing a timeout on slower machines that are not as not as
    powerful as workstations. With this change, the fuzzer is running
    successfully in less than ~30 ms.
    
    BUG=chromium:1112106
    TEST="/usr/libexec/fuzzers/fetch_system_info_fuzzer
    /tmp/clusterfuzz-testcase-minimized-fetch_system_info_fuzzer-5846721078296576"
    "/usr/libexec/fuzzers/fetch_system_info_fuzzer
    /tmp/clusterfuzz-testcase-minimized-fetch_system_info_fuzzer-6029567189385216"
    
    Change-Id: I7a1dbdf647c0af23fa2c3e45abc8d08c4390a480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2337818
    Reviewed-by: Manoj Gupta <manojgupta@chromium.org>
    Reviewed-by: Paul Moy <pmoy@chromium.org>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Tested-by: Kartik Hegde <khegde@chromium.org>

--- diagnostics/cros_healthd/fetchers/fetch_system_info_fuzzer.cc
+++ diagnostics/cros_healthd/fetchers/fetch_system_info_fuzzer.cc
@@ -8,13 +8,53 @@
 #include <string>
 
 #include <base/files/file_path.h>
+#include <base/files/scoped_temp_dir.h>
 #include <base/logging.h>
 #include <chromeos/chromeos-config/libcros_config/fake_cros_config.h>
+#include <fuzzer/FuzzedDataProvider.h>
 
+#include "diagnostics/common/file_test_utils.h"
 #include "diagnostics/cros_healthd/fetchers/system_fetcher.h"
 #include "diagnostics/cros_healthd/system/mock_context.h"
 
 namespace diagnostics {
+
+namespace {
+
+void SetUpSystemFiles(const base::FilePath& root_dir,
+                      FuzzedDataProvider* provider) {
+  // Populate fake cached VPD values.
+  base::FilePath relative_vpd_rw_dir = root_dir.Append(kRelativeVpdRwPath);
+  CHECK(WriteFileAndCreateParentDirs(
+      relative_vpd_rw_dir.Append(kFirstPowerDateFileName),
+      provider->ConsumeRandomLengthString()));
+  base::FilePath relative_vpd_ro_dir = root_dir.Append(kRelativeVpdRoPath);
+  CHECK(WriteFileAndCreateParentDirs(
+      relative_vpd_ro_dir.Append(kManufactureDateFileName),
+      provider->ConsumeRandomLengthString()));
+  CHECK(WriteFileAndCreateParentDirs(
+      relative_vpd_ro_dir.Append(kSkuNumberFileName),
+      provider->ConsumeRandomLengthString()));
+  // Populate fake DMI values.
+  base::FilePath relative_dmi_info_path = root_dir.Append(kRelativeDmiInfoPath);
+  CHECK(WriteFileAndCreateParentDirs(
+      relative_dmi_info_path.Append(kBiosVersionFileName),
+      provider->ConsumeRandomLengthString()));
+  CHECK(WriteFileAndCreateParentDirs(
+      relative_dmi_info_path.Append(kBoardNameFileName),
+      provider->ConsumeRandomLengthString()));
+  CHECK(WriteFileAndCreateParentDirs(
+      relative_dmi_info_path.Append(kBoardVersionFileName),
+      provider->ConsumeRandomLengthString()));
+  CHECK(WriteFileAndCreateParentDirs(
+      relative_dmi_info_path.Append(kChassisTypeFileName),
+      provider->ConsumeRandomLengthString()));
+  CHECK(WriteFileAndCreateParentDirs(
+      relative_dmi_info_path.Append(kProductNameFileName),
+      provider->ConsumeRandomLengthString()));
+}
+
+}  // namespace
 
 class Environment {
  public:
@@ -26,15 +66,26 @@ class Environment {
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static Environment env;
 
-  // Generate a random string.
-  std::string file_path(data, data + size);
+  // 100 KiB max input size. Doing multiple writes and read for significantly
+  // large files can potentially cause the fuzzer to timeout.
+  constexpr int kMaxInputSize = 102400;
+  if (size > kMaxInputSize)
+    return 0;
+
+  FuzzedDataProvider provider(data, size);
+  base::ScopedTempDir temp_dir;
+  // Setup temp dir for writing out the system files the fetcher will read
+  // from.
+  CHECK(temp_dir.CreateUniqueTempDir());
+  SetUpSystemFiles(temp_dir.GetPath(), &provider);
 
   MockContext mock_context;
   mock_context.Initialize();
   mock_context.fake_system_config()->SetHasSkuNumber(true);
   mock_context.fake_system_config()->SetMarketingName("fake_marketing_name");
   SystemFetcher system_fetcher{&mock_context};
-  auto system_info = system_fetcher.FetchSystemInfo(base::FilePath(file_path));
+  auto system_info =
+      system_fetcher.FetchSystemInfo(base::FilePath(temp_dir.GetPath()));
 
   return 0;
 }
