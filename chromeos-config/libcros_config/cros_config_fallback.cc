cc9150564a210876998e43a94380d3a937406e29
Trent Begin <tbegin@google.com>
2020-08-10 17:09:55 -0600

    chromeos-config: add has-backlight to cros_config_fallback
    This also adds a new type of method to populate ConfigFS by using a
    function call instead of just command line output.
    
    BUG=chromium:1114358
    TEST=emerge-bob chromeos-config-tools
    cros deploy dut chromeos-config-tools
    (dut) cat /run/chromeos-config/v1/cros-healthd/has-backlight (true)
    
    Change-Id: I701ee148a1527a1de984f6a6c2bdbf3acae164ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2347141
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Tested-by: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>

--- chromeos-config/libcros_config/cros_config_fallback.cc
+++ chromeos-config/libcros_config/cros_config_fallback.cc
@@ -11,87 +11,122 @@
 #include <string>
 #include <vector>
 
+#include <base/callback.h>
 #include <base/files/file_path.h>
 #include <base/files/file_util.h>
+#include <base/optional.h>
 #include <base/process/launch.h>
 #include <base/strings/string_split.h>
+#include <base/system/sys_info.h>
 #include <brillo/file_utils.h>
 #include "chromeos-config/libcros_config/cros_config_interface.h"
 
+namespace brillo {
+
 namespace {
 
-struct CommandMapEntry {
+struct FunctionMapEntry {
   // The path and property to match on
   const char* path;
   const char* property;
 
-  // The corresponding command to run, which is just a space-separated
-  // argv (not parsed by shell)
-  const char* command;
+  // The function to run to generate the contents for the property.
+  base::RepeatingCallback<base::Optional<std::string>()> function;
 };
 
-constexpr CommandMapEntry kCommandMap[] = {
-  {"/firmware", "image-name", "mosys platform model"},
-  {"/", "name", "mosys platform model"},
-  {"/", "brand-code", "mosys platform brand"},
-  {"/identity", "sku-id", "mosys platform sku"},
-  {"/identity", "platform-name", "mosys platform name"},
-  {"/hardware-properties", "psu-type", "mosys psu type"}};
+// Helper function to determine if the device has a backlight.
+base::Optional<std::string> GetHasBacklight() {
+  // Assume the device has a backlight unless it is a CHROMEBOX or CHROMEBIT.
+  std::string device_type;
+  if (!base::SysInfo::GetLsbReleaseValue("DEVICETYPE", &device_type)) {
+    CROS_CONFIG_LOG(ERROR) << "Unable to get DEVICETYPE from /etc/lsb-release";
+    return base::nullopt;
+  }
+
+  if (device_type == "CHROMEBOX" || device_type == "CHROMEBIT") {
+    return "false";
+  }
+
+  return "true";
+}
+
+// Helper function to run a provided command and return the result on success.
+// |command| is just a space-separated argv (not parsed by shell)
+base::Optional<std::string> GetOutputForCommand(const std::string& command) {
+  std::string result;
+  std::vector<std::string> argv = base::SplitString(
+      command, " ", base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL);
+
+  if (!base::GetAppOutput(argv, &result)) {
+    CROS_CONFIG_LOG(ERROR) << "\"" << command << "\" has non-zero exit code";
+    return base::nullopt;
+  }
+
+  // Trim off (one) trailing newline from command response.
+  if (result.back() == '\n')
+    result.pop_back();
+  return result;
+}
+
+const FunctionMapEntry kFunctionMap[] = {
+    {"/firmware", "image-name",
+     base::BindRepeating(&GetOutputForCommand, "mosys platform model")},
+    {"/", "name",
+     base::BindRepeating(&GetOutputForCommand, "mosys platform model")},
+    {"/", "brand-code",
+     base::BindRepeating(&GetOutputForCommand, "mosys platform brand")},
+    {"/identity", "sku-id",
+     base::BindRepeating(&GetOutputForCommand, "mosys platform sku")},
+    {"/identity", "platform-name",
+     base::BindRepeating(&GetOutputForCommand, "mosys platform name")},
+    {"/hardware-properties", "psu-type",
+     base::BindRepeating(&GetOutputForCommand, "mosys psu type")},
+    {"/cros-healthd", "has-backlight", base::BindRepeating(&GetHasBacklight)}};
+
+// Helper function to write a single value to ConfigFS at the given path.
+// Returns true if successful and false otherwise.
+bool WriteConfigValue(const base::FilePath& output_dir,
+                      const std::string& path,
+                      const std::string& property,
+                      const std::string& value) {
+  auto path_dir = output_dir;
+  for (const auto& part : base::SplitStringPiece(
+           path, "/", base::KEEP_WHITESPACE, base::SPLIT_WANT_NONEMPTY)) {
+    path_dir = path_dir.Append(part);
+  }
+  if (!MkdirRecursively(path_dir, 0755).is_valid()) {
+    CROS_CONFIG_LOG(ERROR) << "Unable to create directory " << path_dir.value()
+                           << ": "
+                           << logging::SystemErrorCodeToString(
+                                  logging::GetLastSystemErrorCode());
+    return false;
+  }
+
+  const auto property_file = path_dir.Append(property);
+  if (base::WriteFile(property_file, value.data(), value.length()) < 0) {
+    CROS_CONFIG_LOG(ERROR) << "Unable to create file " << property_file.value();
+    return false;
+  }
+
+  return true;
+}
 
 }  // namespace
-
-namespace brillo {
 
 CrosConfigFallback::CrosConfigFallback() {}
 CrosConfigFallback::~CrosConfigFallback() {}
 
-static bool GetStringForEntry(const struct CommandMapEntry& entry,
-                              std::string* val_out) {
-  std::vector<std::string> argv = base::SplitString(
-      entry.command, " ", base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL);
-
-  if (!base::GetAppOutput(argv, val_out)) {
-    CROS_CONFIG_LOG(ERROR) << "\"" << entry.command
-                           << "\" has non-zero exit code";
-    return false;
-  }
-
-  // Trim off (one) trailing newline from mosys
-  if (val_out->back() == '\n')
-    val_out->pop_back();
-  return true;
-}
-
 bool CrosConfigFallback::WriteConfigFS(const base::FilePath& output_dir) {
-  for (auto entry : kCommandMap) {
-    std::string value;
-    if (!GetStringForEntry(entry, &value)) {
-      // Not all mosys commands may be supported on every board. Don't
-      // write the property if the board does not support it.
+  for (auto entry : kFunctionMap) {
+    auto value = entry.function.Run();
+    // Not all commands may be supported on every board. Don't
+    // write the property if the board does not support it.
+    if (!value)
       continue;
-    }
 
-    auto path_dir = output_dir;
-    for (const auto& part :
-         base::SplitStringPiece(entry.path, "/", base::KEEP_WHITESPACE,
-                                base::SPLIT_WANT_NONEMPTY)) {
-      path_dir = path_dir.Append(part);
-    }
-
-    if (!MkdirRecursively(path_dir, 0755).is_valid()) {
-      CROS_CONFIG_LOG(ERROR)
-          << "Unable to create directory " << path_dir.value() << ": "
-          << logging::SystemErrorCodeToString(
-                 logging::GetLastSystemErrorCode());
+    if (!WriteConfigValue(output_dir, entry.path, entry.property,
+                          value.value()))
       return false;
-    }
-
-    const auto property_file = path_dir.Append(entry.property);
-    if (base::WriteFile(property_file, value.data(), value.length()) < 0) {
-      CROS_CONFIG_LOG(ERROR)
-          << "Unable to create file " << property_file.value();
-      return false;
-    }
   }
   return true;
 }
