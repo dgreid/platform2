9748dd51d1185b67bd141db151809c6d912c2c41
Yi Chou <yich@google.com>
2020-08-14 14:41:55 +0800

    tpm_manager: correct the copyright header
    BUG=b:146525460
    TEST=build ok.
    
    Change-Id: I564354b8f30b48a435804185d7d7042cfc85f60d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2355535
    Tested-by: joe Chou <yich@google.com>
    Reviewed-by: Leo Lai <cylai@google.com>
    Auto-Submit: joe Chou <yich@google.com>
    Commit-Queue: joe Chou <yich@google.com>

--- tpm_manager/server/nv_index_authenticator.h
+++ tpm_manager/server/nv_index_authenticator.h
@@ -1,9 +1,6 @@
-//
-// Copyright (c) 2018 The Chromium OS Authors. All rights reserved.
-//
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
 
 #ifndef TPM_MANAGER_SERVER_NV_INDEX_AUTHENTICATOR_H_
 #define TPM_MANAGER_SERVER_NV_INDEX_AUTHENTICATOR_H_
