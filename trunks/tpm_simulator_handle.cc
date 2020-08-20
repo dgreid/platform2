0f8030af46d2223c786a506dd078cb07a880985a
Alberto Herrera <ahrfgb@google.com>
2020-08-20 06:07:49 +0000

    trunks: Manufacture the tpm2 simulator only once.
    A check to verify the simulator has already been initialized is done in SendCommandAndWait(), but not in Init(). This is causing the simulator to be “created” more than once. Calling Manufacture(true) multiple times resets the simulated NVRam, loosing the information from any previous session.
    
    BUG=b:132144919
    TEST=Using trunks_client.cc, took ownership of the device and initialized the tpm. The owner password is retained after reboot. trunks_client functionality was locally extended to inject a fake certificate, which is also retained after reboot.
    
    Cq-Depend: chromium:2366414
    Change-Id: Ic7efadd0b24ea6bdf6c1d47882685e81656be06a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2366016
    Tested-by: Alberto Herrera <ahrfgb@google.com>
    Commit-Queue: Alberto Herrera <ahrfgb@google.com>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>

--- trunks/tpm_simulator_handle.cc
+++ trunks/tpm_simulator_handle.cc
@@ -11,6 +11,7 @@ extern "C" {
 #include <tpm2/_TPM_Init_fp.h>
 #include <tpm2/BaseTypes.h>
 #include <tpm2/ExecCommand_fp.h>
+#include <tpm2/tpm_manufacture.h>
 #include <tpm2/TpmBuildSwitches.h>
 #include <tpm2/Manufacture_fp.h>  // NOLINT(build/include_alpha) - needs TpmBuildSwitches.h
 #include <tpm2/Platform.h>
@@ -37,18 +38,34 @@ TpmSimulatorHandle::~TpmSimulatorHandle() {}
 
 bool TpmSimulatorHandle::Init() {
   CHECK_EQ(chdir(kSimulatorStateDirectory), 0);
-  InitializeSimulator();
+  if (!init_) {
+    InitializeSimulator();
+    init_ = true;
+  }
   return true;
 }
 
 void TpmSimulatorHandle::InitializeSimulator() {
 #if defined(USE_SIMULATOR)
   // Initialize TPM.
-  TPM_Manufacture(TRUE);
-  _plat__SetNvAvail();
   _plat__Signal_PowerOn();
+  /*
+   * Make sure NV RAM metadata is initialized, needed to check
+   * manufactured status. This is a speculative call which will have to
+   * be repeated in case the TPM has not been through the manufacturing
+   * sequence yet. No harm in calling it twice in that case.
+   */
   _TPM_Init();
-  LOG(INFO) << "Simulator initialized.";
+  _plat__SetNvAvail();
+
+  if (!tpm_manufactured()) {
+    TPM_Manufacture(TRUE);
+    // TODO(b/132145000): Verify if the second call to _TPM_Init() is necessary.
+    _TPM_Init();
+    if (!tpm_endorse())
+      LOG(ERROR) << __func__ << " Failed to endorse TPM with a fixed key.";
+  }
+
   // Send TPM2_Startup(TPM_SU_CLEAR), ignore the result. This is normally done
   // by firmware. Without TPM2_Startup, TpmUtility::CheckState() fails,
   // ResourceManager aborts initialization, and trunks daemon dies.
