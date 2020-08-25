f906b87bb4d8d32504e4008478339f5c101145d7
Tom Hughes <tomhughes@chromium.org>
2020-08-24 18:18:56 -0700

    wifi-testbed: run clang-format on all files
    These files will fail the "repo upload" lint check unless they have the
    correct format. Now that we're enforcing presubmit check server-side,
    let's format everything correctly so that future changes are not bitten
    by existing format errors.
    
    Command used:
    clang-format -i -style=file $(find wifi-testbed -name '*.h' -o -name '*.cc')
    
    BUG=b:166166797
    TEST=CQ passes
    
    Change-Id: If80e245bc1cdf28adf1b4f558295c0f305b56549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2373650
    Commit-Queue: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Auto-Submit: Tom Hughes <tomhughes@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- wifi-testbed/testbed-regulatory/ieee80211_header.h
+++ wifi-testbed/testbed-regulatory/ieee80211_header.h
@@ -9,52 +9,55 @@
  * the use of them under the terms of a BSD license.
  */
 
+#ifndef WIFI_TESTBED_TESTBED_REGULATORY_IEEE80211_HEADER_H_
+#define WIFI_TESTBED_TESTBED_REGULATORY_IEEE80211_HEADER_H_
+
 #include <linux/types.h>
 
-#define WLAN_FC_TYPE_MGMT               0
-#define WLAN_FC_TYPE_CTRL               1
-#define WLAN_FC_TYPE_DATA               2
+#define WLAN_FC_TYPE_MGMT 0
+#define WLAN_FC_TYPE_CTRL 1
+#define WLAN_FC_TYPE_DATA 2
 
 /* management */
-#define WLAN_FC_STYPE_ASSOC_REQ         0
-#define WLAN_FC_STYPE_ASSOC_RESP        1
-#define WLAN_FC_STYPE_REASSOC_REQ       2
-#define WLAN_FC_STYPE_REASSOC_RESP      3
-#define WLAN_FC_STYPE_PROBE_REQ         4
-#define WLAN_FC_STYPE_PROBE_RESP        5
-#define WLAN_FC_STYPE_BEACON            8
-#define WLAN_FC_STYPE_ATIM              9
-#define WLAN_FC_STYPE_DISASSOC          10
-#define WLAN_FC_STYPE_AUTH              11
-#define WLAN_FC_STYPE_DEAUTH            12
-#define WLAN_FC_STYPE_ACTION            13
+#define WLAN_FC_STYPE_ASSOC_REQ 0
+#define WLAN_FC_STYPE_ASSOC_RESP 1
+#define WLAN_FC_STYPE_REASSOC_REQ 2
+#define WLAN_FC_STYPE_REASSOC_RESP 3
+#define WLAN_FC_STYPE_PROBE_REQ 4
+#define WLAN_FC_STYPE_PROBE_RESP 5
+#define WLAN_FC_STYPE_BEACON 8
+#define WLAN_FC_STYPE_ATIM 9
+#define WLAN_FC_STYPE_DISASSOC 10
+#define WLAN_FC_STYPE_AUTH 11
+#define WLAN_FC_STYPE_DEAUTH 12
+#define WLAN_FC_STYPE_ACTION 13
 
 /* control */
-#define WLAN_FC_STYPE_PSPOLL            10
-#define WLAN_FC_STYPE_RTS               11
-#define WLAN_FC_STYPE_CTS               12
-#define WLAN_FC_STYPE_ACK               13
-#define WLAN_FC_STYPE_CFEND             14
-#define WLAN_FC_STYPE_CFENDACK          15
+#define WLAN_FC_STYPE_PSPOLL 10
+#define WLAN_FC_STYPE_RTS 11
+#define WLAN_FC_STYPE_CTS 12
+#define WLAN_FC_STYPE_ACK 13
+#define WLAN_FC_STYPE_CFEND 14
+#define WLAN_FC_STYPE_CFENDACK 15
 
 /* data */
-#define WLAN_FC_STYPE_DATA              0
-#define WLAN_FC_STYPE_DATA_CFACK        1
-#define WLAN_FC_STYPE_DATA_CFPOLL       2
-#define WLAN_FC_STYPE_DATA_CFACKPOLL    3
-#define WLAN_FC_STYPE_NULLFUNC          4
-#define WLAN_FC_STYPE_CFACK             5
-#define WLAN_FC_STYPE_CFPOLL            6
-#define WLAN_FC_STYPE_CFACKPOLL         7
-#define WLAN_FC_STYPE_QOS_DATA          8
-#define WLAN_FC_STYPE_QOS_DATA_CFACK    9
-#define WLAN_FC_STYPE_QOS_DATA_CFPOLL   10
-#define WLAN_FC_STYPE_QOS_DATA_CFACKPOLL        11
-#define WLAN_FC_STYPE_QOS_NULL          12
-#define WLAN_FC_STYPE_QOS_CFPOLL        14
-#define WLAN_FC_STYPE_QOS_CFACKPOLL     15
+#define WLAN_FC_STYPE_DATA 0
+#define WLAN_FC_STYPE_DATA_CFACK 1
+#define WLAN_FC_STYPE_DATA_CFPOLL 2
+#define WLAN_FC_STYPE_DATA_CFACKPOLL 3
+#define WLAN_FC_STYPE_NULLFUNC 4
+#define WLAN_FC_STYPE_CFACK 5
+#define WLAN_FC_STYPE_CFPOLL 6
+#define WLAN_FC_STYPE_CFACKPOLL 7
+#define WLAN_FC_STYPE_QOS_DATA 8
+#define WLAN_FC_STYPE_QOS_DATA_CFACK 9
+#define WLAN_FC_STYPE_QOS_DATA_CFPOLL 10
+#define WLAN_FC_STYPE_QOS_DATA_CFACKPOLL 11
+#define WLAN_FC_STYPE_QOS_NULL 12
+#define WLAN_FC_STYPE_QOS_CFPOLL 14
+#define WLAN_FC_STYPE_QOS_CFACKPOLL 15
 
-#define WLAN_FC_GET_STYPE(fc) (((fc) & 0x00f0) >> 4)
+#define WLAN_FC_GET_STYPE(fc) (((fc)&0x00f0) >> 4)
 
 /* Action frame categories (IEEE 802.11-2007, 7.3.1.11, Table 7-24) */
 #define WLAN_ACTION_SPECTRUM_MGMT 0
@@ -131,147 +134,145 @@
 #define WLAN_CHANNEL_SWITCH_MODE_XMIT_FORBIDDEN 1
 
 struct ieee80211_hdr {
-        __le16 frame_control;
-        __le16 duration_id;
-        __u8 addr1[6];
-        __u8 addr2[6];
-        __u8 addr3[6];
-        __le16 seq_ctrl;
-        /* followed by 'u8 addr4[6];' if ToDS and FromDS is set in data frame
-         */
+  __le16 frame_control;
+  __le16 duration_id;
+  __u8 addr1[6];
+  __u8 addr2[6];
+  __u8 addr3[6];
+  __le16 seq_ctrl;
+  /* followed by 'u8 addr4[6];' if ToDS and FromDS is set in data frame
+   */
 } __attribute__((packed));
 
 #define IEEE80211_FC(type, stype) ((type << 2) | (stype << 4))
 #define WLAN_SA_QUERY_TR_ID_LEN 2
 
-
 struct ieee80211_mgmt {
-        __le16 frame_control;
-        __le16 duration;
-        __u8 da[6];
-        __u8 sa[6];
-        __u8 bssid[6];
-        __le16 seq_ctrl;
-        union {
-                struct {
-                        __le16 auth_alg;
-                        __le16 auth_transaction;
-                        __le16 status_code;
-                        /* possibly followed by Challenge text */
-                        __u8 variable[0];
-                } __attribute__((packed)) auth;
-                struct {
-                        __le16 reason_code;
-                        __u8 variable[0];
-                } __attribute__((packed)) deauth;
-                struct {
-                        __le16 capab_info;
-                        __le16 listen_interval;
-                        /* followed by SSID and Supported rates */
-                        __u8 variable[0];
-                } __attribute__((packed)) assoc_req;
-                struct {
-                        __le16 capab_info;
-                        __le16 status_code;
-                        __le16 aid;
-                        /* followed by Supported rates */
-                        __u8 variable[0];
-                } __attribute__((packed)) assoc_resp, reassoc_resp;
-                struct {
-                        __le16 capab_info;
-                        __le16 listen_interval;
-                        __u8 current_ap[6];
-                        /* followed by SSID and Supported rates */
-                        __u8 variable[0];
-                } __attribute__((packed)) reassoc_req;
-                struct {
-                        __le16 reason_code;
-                        __u8 variable[0];
-                } __attribute__((packed)) disassoc;
-                struct {
-                        __u8 timestamp[8];
-                        __le16 beacon_int;
-                        __le16 capab_info;
-                        /* followed by some of SSID, Supported rates,
-                         * FH Params, DS Params, CF Params, IBSS Params, TIM */
-                        __u8 variable[0];
-                } __attribute__((packed)) beacon;
-                struct {
-                        /* only variable items: SSID, Supported rates */
-                        __u8 variable[0];
-                } __attribute__((packed)) probe_req;
-                struct {
-                        __u8 timestamp[8];
-                        __le16 beacon_int;
-                        __le16 capab_info;
-                        /* followed by some of SSID, Supported rates,
-                         * FH Params, DS Params, CF Params, IBSS Params */
-                        __u8 variable[0];
-                } __attribute__((packed)) probe_resp;
-                struct {
-                        __u8 category;
-                        union {
-                                struct {
-                                        __u8 action_code;
-                                        __u8 dialog_token;
-                                        __u8 status_code;
-                                        __u8 variable[0];
-                                } __attribute__((packed)) wmm_action;
-                                struct{
-                                        __u8 action_code;
-                                        __u8 element_id;
-                                        __u8 length;
-                                        __u8 switch_mode;
-                                        __u8 new_chan;
-                                        __u8 switch_count;
-                                } __attribute__((packed)) chan_switch;
-                                struct {
-                                        __u8 action;
-                                        __u8 sta_addr[ETH_ALEN];
-                                        __u8 target_ap_addr[ETH_ALEN];
-                                        __u8 variable[0]; /* FT Request */
-                                } __attribute__((packed)) ft_action_req;
-                                struct {
-                                        __u8 action;
-                                        __u8 sta_addr[ETH_ALEN];
-                                        __u8 target_ap_addr[ETH_ALEN];
-                                        __le16 status_code;
-                                        __u8 variable[0]; /* FT Request */
-                                } __attribute__((packed)) ft_action_resp;
-                                struct {
-                                        __u8 action;
-                                        __u8 trans_id[WLAN_SA_QUERY_TR_ID_LEN];
-                                } __attribute__((packed)) sa_query_req;
-                                struct {
-                                        __u8 action; /* */
-                                        __u8 trans_id[WLAN_SA_QUERY_TR_ID_LEN];
-                                } __attribute__((packed)) sa_query_resp;
-                                struct {
-                                        __u8 action;
-                                        __u8 variable[0];
-                                } __attribute__((packed)) public_action;
-                                struct {
-                                        __u8 action; /* 9 */
-                                        __u8 oui[3];
-                                        /* Vendor-specific content */
-                                        __u8 variable[0];
-                                } __attribute__((packed)) vs_public_action;
-                                struct {
-                                        __u8 action; /* 7 */
-                                        __u8 dialog_token;
-                                        __u8 req_mode;
-                                        __le16 disassoc_timer;
-                                        __u8 validity_interval;
-                                        /* BSS Termination Duration (optional),
-                                         * Session Information URL (optional),
-                                         * BSS Transition Candidate List
-                                         * Entries */
-                                        __u8 variable[0];
-                                } __attribute__((packed)) bss_tm_req;
-                        } u;
-                } __attribute__((packed)) action;
-        } u;
+  __le16 frame_control;
+  __le16 duration;
+  __u8 da[6];
+  __u8 sa[6];
+  __u8 bssid[6];
+  __le16 seq_ctrl;
+  union {
+    struct {
+      __le16 auth_alg;
+      __le16 auth_transaction;
+      __le16 status_code;
+      /* possibly followed by Challenge text */
+      __u8 variable[0];
+    } __attribute__((packed)) auth;
+    struct {
+      __le16 reason_code;
+      __u8 variable[0];
+    } __attribute__((packed)) deauth;
+    struct {
+      __le16 capab_info;
+      __le16 listen_interval;
+      /* followed by SSID and Supported rates */
+      __u8 variable[0];
+    } __attribute__((packed)) assoc_req;
+    struct {
+      __le16 capab_info;
+      __le16 status_code;
+      __le16 aid;
+      /* followed by Supported rates */
+      __u8 variable[0];
+    } __attribute__((packed)) assoc_resp, reassoc_resp;
+    struct {
+      __le16 capab_info;
+      __le16 listen_interval;
+      __u8 current_ap[6];
+      /* followed by SSID and Supported rates */
+      __u8 variable[0];
+    } __attribute__((packed)) reassoc_req;
+    struct {
+      __le16 reason_code;
+      __u8 variable[0];
+    } __attribute__((packed)) disassoc;
+    struct {
+      __u8 timestamp[8];
+      __le16 beacon_int;
+      __le16 capab_info;
+      /* followed by some of SSID, Supported rates,
+       * FH Params, DS Params, CF Params, IBSS Params, TIM */
+      __u8 variable[0];
+    } __attribute__((packed)) beacon;
+    struct {
+      /* only variable items: SSID, Supported rates */
+      __u8 variable[0];
+    } __attribute__((packed)) probe_req;
+    struct {
+      __u8 timestamp[8];
+      __le16 beacon_int;
+      __le16 capab_info;
+      /* followed by some of SSID, Supported rates,
+       * FH Params, DS Params, CF Params, IBSS Params */
+      __u8 variable[0];
+    } __attribute__((packed)) probe_resp;
+    struct {
+      __u8 category;
+      union {
+        struct {
+          __u8 action_code;
+          __u8 dialog_token;
+          __u8 status_code;
+          __u8 variable[0];
+        } __attribute__((packed)) wmm_action;
+        struct {
+          __u8 action_code;
+          __u8 element_id;
+          __u8 length;
+          __u8 switch_mode;
+          __u8 new_chan;
+          __u8 switch_count;
+        } __attribute__((packed)) chan_switch;
+        struct {
+          __u8 action;
+          __u8 sta_addr[ETH_ALEN];
+          __u8 target_ap_addr[ETH_ALEN];
+          __u8 variable[0]; /* FT Request */
+        } __attribute__((packed)) ft_action_req;
+        struct {
+          __u8 action;
+          __u8 sta_addr[ETH_ALEN];
+          __u8 target_ap_addr[ETH_ALEN];
+          __le16 status_code;
+          __u8 variable[0]; /* FT Request */
+        } __attribute__((packed)) ft_action_resp;
+        struct {
+          __u8 action;
+          __u8 trans_id[WLAN_SA_QUERY_TR_ID_LEN];
+        } __attribute__((packed)) sa_query_req;
+        struct {
+          __u8 action; /* */
+          __u8 trans_id[WLAN_SA_QUERY_TR_ID_LEN];
+        } __attribute__((packed)) sa_query_resp;
+        struct {
+          __u8 action;
+          __u8 variable[0];
+        } __attribute__((packed)) public_action;
+        struct {
+          __u8 action; /* 9 */
+          __u8 oui[3];
+          /* Vendor-specific content */
+          __u8 variable[0];
+        } __attribute__((packed)) vs_public_action;
+        struct {
+          __u8 action; /* 7 */
+          __u8 dialog_token;
+          __u8 req_mode;
+          __le16 disassoc_timer;
+          __u8 validity_interval;
+          /* BSS Termination Duration (optional),
+           * Session Information URL (optional),
+           * BSS Transition Candidate List
+           * Entries */
+          __u8 variable[0];
+        } __attribute__((packed)) bss_tm_req;
+      } u;
+    } __attribute__((packed)) action;
+  } u;
 } __attribute__((packed));
 
-
-
+#endif  // WIFI_TESTBED_TESTBED_REGULATORY_IEEE80211_HEADER_H_
