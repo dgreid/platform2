3916aabb404b4cf66d9f08964b1ec7dd9325dd8c
Tom Hughes <tomhughes@chromium.org>
2020-08-27 15:26:53 -0700

    fitpicker: run clang-format on all files
    Also enable the clang_format_presubmit check.
    
    Command used:
    clang-format -i -style=file $(find fitpicker -name '*.h' -o -name '*.cc' -o -name '*.c')
    
    BUG=b:166166797
    TEST=CQ passes
    
    Change-Id: I53ab2532eab6b0c570e9db936932c621411955e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2380643
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Auto-Submit: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- fitpicker/fitpicker.c
+++ fitpicker/fitpicker.c
@@ -17,7 +17,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
 static const char CONFIG_NODE_PATH[] = "/configurations";
 static const char IMAGES_NODE_PATH[] = "/images";
 
@@ -26,19 +25,18 @@ static const char COMPAT_PROP_NAME[] = "compatible";
 static const char KERNEL_PROP_NAME[] = "kernel";
 static const char DTB_PROP_NAME[] = "fdt";
 
-
 typedef struct Image {
   // Name of the image node.
-  const char *name;
+  const char* name;
   // Pointer to the image data.
-  const void *data;
+  const void* data;
   // Length of the image.
   int len;
 } Image;
 
 typedef struct Config {
   // Name of the config node.
-  const char *name;
+  const char* name;
   // Offset to the config in the FIT.
   int offset;
 
@@ -50,21 +48,19 @@ typedef struct Config {
   // Used when comparing multiple configs that match a compat string.
   int rank;
 
-  struct Config *next;
+  struct Config* next;
 } Config;
 
-
 // A linked list of configurations found in the FIT file.
-static Config *fp_configs = NULL;
+static Config* fp_configs = NULL;
 
 // Whether verbose output has been enabled.
 static int fp_verbose = 0;
 
-
 // Prints informational messages to stdout if verbose output has been enabled.
 // Accepts the same arguments as printf.
 static void __attribute__((__format__(__printf__, 1, 2)))
-    fp_log(const char *format, ...) {
+fp_log(const char* format, ...) {
   va_list args;
 
   if (fp_verbose) {
@@ -76,22 +72,22 @@ static void __attribute__((__format__(__printf__, 1, 2)))
 
 // Usage information for the program.
 static void fp_print_usage(void) {
-  printf("Usage: %s [options] <fit> <compat> <kernel> <dtb>\n"
-         "\n"
-         "Options:\n"
-         "\t-v:\tVerbose output.\n"
-         "\t-h:\tShow this help.\n"
-         "\n"
-         "fit:\tPath to the FIT file to pick a kernel/dtb from.\n"
-         "compat:\tThe \"compat\" property to search for.\n"
-         "kernel:\tFile name to write the chosen kernel to.\n"
-         "dtb:\tFile name to write the chosen device tree to.\n",
-         program_invocation_short_name);
+  printf(
+      "Usage: %s [options] <fit> <compat> <kernel> <dtb>\n"
+      "\n"
+      "Options:\n"
+      "\t-v:\tVerbose output.\n"
+      "\t-h:\tShow this help.\n"
+      "\n"
+      "fit:\tPath to the FIT file to pick a kernel/dtb from.\n"
+      "compat:\tThe \"compat\" property to search for.\n"
+      "kernel:\tFile name to write the chosen kernel to.\n"
+      "dtb:\tFile name to write the chosen device tree to.\n",
+      program_invocation_short_name);
 }
 
-
 // Allocate a buffer and read the contents of a file into it.
-static const void *fp_read_file(const char *path) {
+static const void* fp_read_file(const char* path) {
   const int fd = open(path, 0, O_RDONLY | O_CLOEXEC);
   if (fd < 0) {
     warn("Failed to open %s for reading.", path);
@@ -107,14 +103,14 @@ static const void *fp_read_file(const char *path) {
   const off_t size = stat_buf.st_size;
 
   // Allocate a buffer.
-  void *buf = malloc(size);
+  void* buf = malloc(size);
   if (!buf) {
-    warn("Couldn't allocate %"PRIi64" bytes.", size);
+    warn("Couldn't allocate %" PRIi64 " bytes.", size);
     return NULL;
   }
 
   // Read data into it until we hit an error or get the whole file.
-  uint8_t *ptr = buf;
+  uint8_t* ptr = buf;
   off_t left = size;
   do {
     ssize_t ret = read(fd, ptr, left);
@@ -131,17 +127,16 @@ static const void *fp_read_file(const char *path) {
 }
 
 // Write data in a buffer into a file.
-static int fp_write_file(const char *path, const void *data, off_t len) {
-  const int fd = open(path,
-                      O_CREAT | O_TRUNC | O_WRONLY | O_CLOEXEC | O_NOFOLLOW,
-                      0644);
+static int fp_write_file(const char* path, const void* data, off_t len) {
+  const int fd =
+      open(path, O_CREAT | O_TRUNC | O_WRONLY | O_CLOEXEC | O_NOFOLLOW, 0644);
   if (fd < 0) {
     warn("Failed to open %s for writing.", path);
     return -1;
   }
 
   // Write data until we hit an error or write the whole file.
-  const uint8_t *ptr = data;
+  const uint8_t* ptr = data;
   off_t left = len;
   do {
     int ret = write(fd, ptr, left);
@@ -156,11 +151,11 @@ static int fp_write_file(const char *path, const void *data, off_t len) {
   return len;
 }
 
-
 // Read an image with a given name from the image nodes in the FIT image.
 // If lenp is non-NULL, it'll be set to the length of the image data.
-static const void *fp_read_image(const void *fit_buf, const char *name,
-                                 int *lenp) {
+static const void* fp_read_image(const void* fit_buf,
+                                 const char* name,
+                                 int* lenp) {
   // Find the parent node which houses all the image nodes.
   int images_offset = fdt_path_offset(fit_buf, IMAGES_NODE_PATH);
   if (images_offset < 0) {
@@ -176,8 +171,8 @@ static const void *fp_read_image(const void *fit_buf, const char *name,
   }
 
   // Get the "data" property from it which holds image data.
-  const struct fdt_property *image_prop =
-    fdt_get_property(fit_buf, image_offset, DATA_PROP_NAME, lenp);
+  const struct fdt_property* image_prop =
+      fdt_get_property(fit_buf, image_offset, DATA_PROP_NAME, lenp);
   if (!image_prop) {
     warn("Failed to read image data for %s.", name);
     return NULL;
@@ -187,9 +182,9 @@ static const void *fp_read_image(const void *fit_buf, const char *name,
 }
 
 // Find the "compatible" property at the root of a device tree.
-static const char *fp_get_compat(const void *dtb_buf, int *lenp) {
-  const struct fdt_property *compatible =
-    fdt_get_property(dtb_buf, 0, COMPAT_PROP_NAME, lenp);
+static const char* fp_get_compat(const void* dtb_buf, int* lenp) {
+  const struct fdt_property* compatible =
+      fdt_get_property(dtb_buf, 0, COMPAT_PROP_NAME, lenp);
   if (!compatible) {
     warn("Couldn't find '%s' string.", COMPAT_PROP_NAME);
     return NULL;
@@ -199,7 +194,7 @@ static const char *fp_get_compat(const void *dtb_buf, int *lenp) {
 }
 
 // Find the offset of the first configuration node in the FIT image.
-static int fp_first_config(const void *fit_buf) {
+static int fp_first_config(const void* fit_buf) {
   // Find the parent node which houses all the configurations.
   int configs_offset = fdt_path_offset(fit_buf, CONFIG_NODE_PATH);
   if (configs_offset < 0) {
@@ -217,7 +212,7 @@ static int fp_first_config(const void *fit_buf) {
 
 // Find the offset of the next configuration node after the one at "offset"
 // in a FIT image.
-static int fp_next_config(const void *fit_buf, int offset) {
+static int fp_next_config(const void* fit_buf, int offset) {
   offset = fdt_next_subnode(fit_buf, offset);
   if (offset < 0)
     return 0;
@@ -225,16 +220,17 @@ static int fp_next_config(const void *fit_buf, int offset) {
   return offset;
 }
 
-
 // Fill in info about an image associated with a configuration in the FIT.
-static int fp_init_image(const void *fit_buf, Config *config,
-                         const char *prop_name, Image *image) {
+static int fp_init_image(const void* fit_buf,
+                         Config* config,
+                         const char* prop_name,
+                         Image* image) {
   // Find the property which holds the name of the image of this type.
-  const struct fdt_property *prop =
-    fdt_get_property(fit_buf, config->offset, prop_name, NULL);
+  const struct fdt_property* prop =
+      fdt_get_property(fit_buf, config->offset, prop_name, NULL);
   if (!prop) {
-    warn("Failed to find %s image name for config %s.",
-         prop_name, config->name);
+    warn("Failed to find %s image name for config %s.", prop_name,
+         config->name);
     return -1;
   }
   image->name = prop->data;
@@ -248,8 +244,8 @@ static int fp_init_image(const void *fit_buf, Config *config,
 }
 
 // Initialize and fill out a structure describing a configuration in the FIT.
-static Config *fp_init_config(const void *fit_buf, int offset) {
-  Config *config = malloc(sizeof(Config));
+static Config* fp_init_config(const void* fit_buf, int offset) {
+  Config* config = malloc(sizeof(Config));
   config->next = NULL;
   config->rank = -1;
   config->offset = offset;
@@ -267,17 +263,16 @@ static Config *fp_init_config(const void *fit_buf, int offset) {
   return config;
 }
 
-
-int main(int argc, char * const argv[]) {
+int main(int argc, char* const argv[]) {
   int c;
   while ((c = getopt(argc, argv, "vh")) != -1) {
     switch (c) {
-    case 'v':
-      fp_verbose = 1;
-      break;
-    case 'h':
-      fp_print_usage();
-      return 0;
+      case 'v':
+        fp_verbose = 1;
+        break;
+      case 'h':
+        fp_print_usage();
+        return 0;
     }
   }
 
@@ -286,12 +281,12 @@ int main(int argc, char * const argv[]) {
     return 1;
   }
 
-  const char *fit_path = argv[optind + 0];
-  const char *compat = argv[optind + 1];
-  const char *kernel_path = argv[optind + 2];
-  const char *dtb_path = argv[optind + 3];
+  const char* fit_path = argv[optind + 0];
+  const char* compat = argv[optind + 1];
+  const char* kernel_path = argv[optind + 2];
+  const char* dtb_path = argv[optind + 3];
 
-  const void *fit_buf = fp_read_file(fit_path);
+  const void* fit_buf = fp_read_file(fit_path);
   if (!fit_buf)
     return 1;
 
@@ -300,7 +295,7 @@ int main(int argc, char * const argv[]) {
     return 1;
 
   // Read all the configurations from the FIT into a linked list.
-  for (Config **end = &fp_configs; offset; end = &(*end)->next) {
+  for (Config** end = &fp_configs; offset; end = &(*end)->next) {
     *end = fp_init_config(fit_buf, offset);
     if (!*end)
       return 1;
@@ -311,13 +306,13 @@ int main(int argc, char * const argv[]) {
   // compat string we were given. Best is defined to be the one whose
   // most specific (earliest) compatible property element matches the
   // compat string.
-  Config *best = NULL;
-  for (Config *config = fp_configs; config; config = config->next) {
-    fp_log("Config %s: kernel = %s, dtb = %s.\n",
-           config->name, config->kernel.name, config->dtb.name);
+  Config* best = NULL;
+  for (Config* config = fp_configs; config; config = config->next) {
+    fp_log("Config %s: kernel = %s, dtb = %s.\n", config->name,
+           config->kernel.name, config->dtb.name);
 
     int compat_len;
-    const char *dtb_compat = fp_get_compat(config->dtb.data, &compat_len);
+    const char* dtb_compat = fp_get_compat(config->dtb.data, &compat_len);
     if (!dtb_compat)
       return 1;
 
