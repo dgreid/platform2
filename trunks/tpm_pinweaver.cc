57f858ff2e1470f6ba4caadd157c7954d993c7f1
Allen Webb <allenwebb@google.com>
2020-09-08 12:07:54 -0500

    trunks: Add static_cast to tpm_pinweaver.cc to fix undefined behavior.
    BUG=chromium:1125295
    TEST=FEATURES=test emerge-${BOARD} trunks
    
    Change-Id: I52e4910783748259bf723d550f86a93fbb9d972d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2399060
    Tested-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Allen Webb <allenwebb@google.com>

--- trunks/tpm_pinweaver.cc
+++ trunks/tpm_pinweaver.cc
@@ -380,7 +380,10 @@ TPM_RC Parse_pw_try_auth_t(
   auto itr = buffer.begin() + sizeof(pw_response_header_t);
   // This field may not be aligned so it is retrieved in a way that will work
   // regardless of platform.
-  *seconds_to_wait = (itr[0] << 24) | (itr[1] << 16) | (itr[2] << 8) | itr[3];
+  *seconds_to_wait = (static_cast<uint32_t>(itr[0]) << 24) |
+                     (static_cast<uint32_t>(itr[1]) << 16) |
+                     (static_cast<uint32_t>(itr[2]) << 8) |
+                     static_cast<uint32_t>(itr[3]);
   itr += 4;
 
   // he_secret is only valid for EC_SUCCESS.
