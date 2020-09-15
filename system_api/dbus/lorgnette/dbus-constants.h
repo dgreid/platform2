911fccb56a699eea34c9283bbb82cbea97814515
Fletcher Woodruff <fletcherw@chromium.org>
2020-09-15 11:50:25 -0600

    system_api: add scan dimensions to lorgnette API
    Add a field to the DocumentSource proto which specifies the dimensions
    for the input source (in millimeters).
    
    Add a field to ScanSettings which allows specifying the region to scan
    from.
    
    BUG=b:168826755
    TEST=cq
    
    Change-Id: Iaf49591867b71b0285b39076f0cad7d11f2fe128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2419315
    Commit-Queue: Fletcher Woodruff <fletcherw@chromium.org>
    Tested-by: Fletcher Woodruff <fletcherw@chromium.org>
    Reviewed-by: Benjamin Gordon <bmgordon@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>

--- system_api/dbus/lorgnette/dbus-constants.h
+++ system_api/dbus/lorgnette/dbus-constants.h
@@ -28,6 +28,11 @@ const char kScanPropertyModeColor[] = "Color";
 const char kScanPropertyModeGray[] = "Gray";
 const char kScanPropertyModeLineart[] = "Lineart";
 const char kScanPropertyResolution[] = "Resolution";
+
+// The name used as a source name for the only DocumentSource in
+// ScannerCapabilities' source field if the SANE backend does not export a
+// source option.
+const char kUnspecifiedDefaultSourceName[] = "DefaultSource";
 }  // namespace lorgnette
 
 #endif  // SYSTEM_API_DBUS_LORGNETTE_DBUS_CONSTANTS_H_
