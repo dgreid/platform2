colemickens/platform2-sommelier

commit 10e19de849d0c0e0e7975e4572b50fde46ee3d49

mems_setup: trigger setup for accelerometer
This commits sets up a sysfs trigger for accelerometer devices.
This trigger is a known entry-point used by Chrome to poll the accelerometer
in order to discover device rotation.

BUG=chromium:937916
TEST=cros_workon_make --test

Change-Id: I0319e452ae459639c9231508f631053fed9a22ef
Signed-off-by: Enrico Granata <egranata@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1622769
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>

mems_setup/delegate_impl.cc
@@ -2,7 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <grp.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <memory>
 
 #include <vector>
 
@@ -108,6 +113,40 @@ bool DelegateImpl::ProbeKernelModule(const std::string& module) {
 
 bool DelegateImpl::Exists(const base::FilePath& fp) {
   return base::PathExists(fp);
+}
+
+base::Optional<gid_t> DelegateImpl::FindGroupId(const char* group) {
+  size_t len = 1024;
+  const auto max_len = sysconf(_SC_GETGR_R_SIZE_MAX);
+  if (max_len != -1) len = max_len;
+
+  std::unique_ptr<char> buf(new char[len]);
+  struct group result;
+  struct group *resultp;
+
+  int err = getgrnam_r(group, &result, buf.get(), len, &resultp);
+  if (err)
+    return base::nullopt;
+  else
+    return result.gr_gid;
+}
+
+int DelegateImpl::GetPermissions(const base::FilePath& path) {
+  int mode = 0;
+  bool ok = base::GetPosixFilePermissions(path, &mode);
+  if (ok)
+    return mode;
+  return 0;
+}
+
+bool DelegateImpl::SetPermissions(const base::FilePath& path, int mode) {
+  return base::SetPosixFilePermissions(path, mode);
+}
+
+bool DelegateImpl::SetOwnership(const base::FilePath& path,
+                                uid_t user,
+                                gid_t group) {
+  return lchown(path.value().c_str(), user, group) == 0;
 }
 
 }  // namespace mems_setup

mems_setup/accelerometer_test.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <base/files/file_util.h>
+
 #include <gtest/gtest.h>
 
 #include "mems_setup/configuration.h"
@@ -21,11 +23,15 @@ namespace mems_setup {
 
 namespace {
 
+static gid_t kChronosGroupId = 666;
+
 class AccelerometerTest : public SensorTestBase {
  public:
   AccelerometerTest()
       : SensorTestBase(
-            "cros-ec-accel", "iio:device1", SensorKind::ACCELEROMETER) {}
+            "cros-ec-accel", "iio:device1", SensorKind::ACCELEROMETER) {
+    mock_delegate_->AddGroup("chronos", kChronosGroupId);
+  }
 };
 
 TEST_F(AccelerometerTest, MissingVpd) {
@@ -119,6 +125,18 @@ TEST_F(AccelerometerTest, MultipleSensorDevice) {
       mock_device_->ReadNumberAttribute("in_accel_y_lid_calibbias").value());
   EXPECT_FALSE(mock_device_->ReadNumberAttribute("in_accel_z_lid_calibbias")
                    .has_value());
+}
+
+TEST_F(AccelerometerTest, TriggerPermissions) {
+  SetSingleSensor("lid");
+  EXPECT_TRUE(GetConfiguration()->Configure());
+
+  base::FilePath trigger_now = mock_trigger0_->GetPath().Append("trigger_now");
+  EXPECT_NE(0, mock_delegate_->GetPermissions(trigger_now) &
+                   base::FILE_PERMISSION_WRITE_BY_GROUP);
+  gid_t gid = 0;
+  mock_delegate_->GetOwnership(trigger_now, nullptr, &gid);
+  EXPECT_EQ(kChronosGroupId, gid);
 }
 
 }  // namespace

mems_setup/configuration.cc
@@ -4,6 +4,7 @@
 
 #include <vector>
 
+#include <base/files/file_util.h>
 #include <base/logging.h>
 #include <base/strings/string_number_conversions.h>
 #include <base/strings/stringprintf.h>
@@ -28,7 +29,7 @@ constexpr char kCalibrationBias[] = "bias";
 
 constexpr int kGyroMaxVpdCalibration = 16384;  // 16dps
 constexpr int kAccelMaxVpdCalibration = 256;  // .250g
-
+constexpr int kAccelSysfsTriggerId = 0;
 }  // namespace
 
 Configuration::Configuration(IioDevice* sensor, SensorKind kind, Delegate* del)
@@ -119,6 +120,63 @@ bool Configuration::CopyCalibrationBiasFromVpd(int max_value,
   return true;
 }
 
+bool Configuration::AddSysfsTrigger(int trigger_id) {
+  auto iio_trig = sensor_->GetContext()->GetDevice("iio_sysfs_trigger");
+  if (iio_trig == nullptr) {
+    LOG(ERROR) << "cannot find iio_trig_sysfs kernel module";
+    return false;
+  }
+
+  // There is a potential cross-process race here, where multiple instances
+  // of this tool may be trying to access the trigger at once. To solve this,
+  // first see if the trigger is already there. If not, try to create it, and
+  // then try to access it again. Only if the latter access fails then
+  // error out.
+  std::string trigger_name = base::StringPrintf("trigger%d", trigger_id);
+  auto trigger = sensor_->GetContext()->GetDevice(trigger_name);
+  if (trigger == nullptr) {
+    LOG(INFO) << trigger_name << " not found; adding";
+    if (!iio_trig->WriteNumberAttribute("add_trigger", trigger_id)) {
+      LOG(WARNING) << "cannot instantiate trigger " << trigger_name;
+    }
+
+    sensor_->GetContext()->Reload();
+    trigger = sensor_->GetContext()->GetDevice(trigger_name);
+    if (trigger == nullptr) {
+      LOG(ERROR) << "cannot find trigger device " << trigger_name;
+      return false;
+    }
+  }
+
+  if (!sensor_->SetTrigger(trigger)) {
+    LOG(ERROR) << "cannot set sensor's trigger to " << trigger_name;
+    return false;
+  }
+
+  base::FilePath trigger_now = trigger->GetPath().Append("trigger_now");
+
+  base::Optional<gid_t> chronos_gid = delegate_->FindGroupId("chronos");
+  if (!chronos_gid) {
+    LOG(ERROR) << "chronos group not found";
+    return false;
+  }
+
+  if (!delegate_->SetOwnership(trigger_now, -1, chronos_gid.value())) {
+    LOG(ERROR) << "cannot configure ownership on the trigger";
+    return false;
+  }
+
+  int permission = delegate_->GetPermissions(trigger_now);
+  permission |= base::FILE_PERMISSION_WRITE_BY_GROUP;
+  if (!delegate_->SetPermissions(trigger_now, permission)) {
+    LOG(ERROR) << "cannot configure permissions on the trigger";
+    return false;
+  }
+
+  LOG(INFO) << "sysfs trigger setup complete";
+  return true;
+}
+
 bool Configuration::ConfigGyro() {
   if (!CopyCalibrationBiasFromVpd(kGyroMaxVpdCalibration))
     return false;
@@ -129,6 +187,9 @@ bool Configuration::ConfigGyro() {
 
 bool Configuration::ConfigAccelerometer() {
   if (!CopyCalibrationBiasFromVpd(kAccelMaxVpdCalibration))
+    return false;
+
+  if (!AddSysfsTrigger(kAccelSysfsTriggerId))
     return false;
 
   LOG(INFO) << "accelerometer configuration complete";

mems_setup/configuration.h
@@ -29,6 +29,8 @@ class Configuration {
   bool CopyCalibrationBiasFromVpd(int max_value);
   bool CopyCalibrationBiasFromVpd(int max_value, const std::string& location);
 
+  bool AddSysfsTrigger(int trigger_id);
+
   Delegate* delegate_;  // non-owned
   SensorKind kind_;
   IioDevice* sensor_;  // non-owned

mems_setup/delegate.h
@@ -24,6 +24,15 @@ class Delegate {
 
   virtual bool Exists(const base::FilePath&) = 0;
 
+  virtual base::Optional<gid_t> FindGroupId(const char* group) = 0;
+
+  virtual int GetPermissions(const base::FilePath& path) = 0;
+  virtual bool SetPermissions(const base::FilePath& path, int mode) = 0;
+
+  virtual bool SetOwnership(const base::FilePath& path,
+                            uid_t user,
+                            gid_t group) = 0;
+
  protected:
   Delegate() = default;
 

mems_setup/delegate_impl.h
@@ -26,6 +26,15 @@ class DelegateImpl : public Delegate {
 
   bool Exists(const base::FilePath&) override;
 
+  base::Optional<gid_t> FindGroupId(const char* group) override;
+
+  int GetPermissions(const base::FilePath& path) override;
+  bool SetPermissions(const base::FilePath& path, int mode) override;
+
+  bool SetOwnership(const base::FilePath& path,
+                    uid_t user,
+                    gid_t group) override;
+
  private:
   void LoadVpdIfNeeded();
 

mems_setup/test_mocks.cc
@@ -103,5 +103,44 @@ bool MockDelegate::Exists(const base::FilePath& fp) {
   return false;
 }
 
+base::Optional<gid_t> MockDelegate::FindGroupId(const char* group) {
+  auto k = groups_.find(group);
+  if (k == groups_.end())
+    return base::nullopt;
+  return k->second;
+}
+
+int MockDelegate::GetPermissions(const base::FilePath& path) {
+  auto k = permissions_.find(path.value());
+  if (k == permissions_.end())
+    return 0;
+  return k->second;
+}
+
+bool MockDelegate::SetPermissions(const base::FilePath& path, int mode) {
+  permissions_[path.value()] = mode;
+  return true;
+}
+
+bool MockDelegate::GetOwnership(const base::FilePath& path,
+                                uid_t* user,
+                                gid_t* group) {
+  auto k = ownerships_.find(path.value());
+  if (k == ownerships_.end())
+    return false;
+  if (user)
+    *user = k->second.first;
+  if (group)
+    *group = k->second.second;
+  return true;
+}
+
+bool MockDelegate::SetOwnership(const base::FilePath& path,
+                                uid_t user,
+                                gid_t group) {
+  ownerships_[path.value()] = {user, group};
+  return true;
+}
+
 }  // namespace mocks
 }  // namespace mems_setup

mems_setup/test_mocks.h
@@ -118,9 +118,26 @@ class MockDelegate : public Delegate {
 
   bool Exists(const base::FilePath&) override;
 
+  base::Optional<gid_t> FindGroupId(const char* group) override;
+
+  int GetPermissions(const base::FilePath& path) override;
+  bool SetPermissions(const base::FilePath& path, int mode) override;
+
+  void AddGroup(const std::string& name, gid_t gid) {
+    groups_.emplace(name, gid);
+  }
+
+  bool GetOwnership(const base::FilePath& path, uid_t* user, gid_t* group);
+  bool SetOwnership(const base::FilePath& path,
+                    uid_t user,
+                    gid_t group) override;
+
  private:
   std::vector<std::string> probed_modules_;
   std::map<std::string, std::string> vpd_;
+  std::map<std::string, gid_t> groups_;
+  std::map<std::string, int> permissions_;
+  std::map<std::string, std::pair<uid_t, gid_t>> ownerships_;
 };
 
 }  // namespace mocks

dgreid/platform2

commit a99b5627b83918cf9579af182cbcd31410b4e554

mems_setup: Use resultp value to determine if group was found
In getgrgid_r(), err can be 0 even when a group is not found.
Only **resultp matters: if NULL, no group was found.
Otherwise, the content of result will be the last group.

Fixes: 10e19de849d0 ("mems_setup: trigger setup for accelerometer")

BUG=chromium:937916
TEST=With iioservice group not created, check mems_setup fails.

Change-Id: I870ed87b57121cc282cedb4acfd5211c653ef363
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2406754
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Tested-by: Cheng-Hao Yang <chenghaoyang@chromium.org>
Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
Reviewed-by: Cheng-Hao Yang <chenghaoyang@chromium.org>

mems_setup/delegate_impl.cc
@@ -123,13 +123,13 @@ base::Optional<gid_t> DelegateImpl::FindGroupId(const char* group) {
 
   std::vector<char> buf(len);
   struct group result;
-  struct group* resultp;
+  struct group* resultp = nullptr;
 
-  int err = getgrnam_r(group, &result, buf.data(), len, &resultp);
-  if (err)
+  getgrnam_r(group, &result, buf.data(), len, &resultp);
+  if (!resultp)
     return base::nullopt;
-  else
-    return result.gr_gid;
+
+  return resultp->gr_gid;
 }
 
 int DelegateImpl::GetPermissions(const base::FilePath& path) {