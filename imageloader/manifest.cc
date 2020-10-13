1574c7e1684649351adfa35d2f997bf56998efc0
hscham <hscham@chromium.org>
2020-10-13 12:40:01 +0900

    imageloader: base::Value refactor
    Replace usages of to-be-deprecated base::DictionaryValue by new
    base::Value API.
    
    BUG=chromium:1099111
    TEST=cros_run_unit_tests --board=eve --packages imageloader
    
    Change-Id: I24d903337d89beef7f1f8c1d481b051c51055fbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2470218
    Tested-by: Grace Cham <hscham@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>
    Reviewed-by: ฤทธิเดช พองฤทธิ์ <pongaoi12345po@gmail.com>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- imageloader/manifest.cc
+++ imageloader/manifest.cc
@@ -5,8 +5,9 @@
 #include <memory>
 #include <utility>
 
-#include <base/json/json_string_value_serializer.h>
+#include <base/json/json_reader.h>
 #include <base/strings/string_number_conversions.h>
+#include <base/values.h>
 
 #include "imageloader/manifest.h"
 
@@ -49,23 +50,21 @@ bool GetSHA256FromString(const std::string& hash_str,
 
 // Ensure the metadata entry is a dictionary mapping strings to strings and
 // parse it into |out_metadata| and return true if so.
-bool ParseMetadata(const base::Value* metadata_element,
+bool ParseMetadata(const base::Value& metadata_dict,
                    std::map<std::string, std::string>* out_metadata) {
   DCHECK(out_metadata);
 
-  const base::DictionaryValue* metadata_dict = nullptr;
-  if (!metadata_element->GetAsDictionary(&metadata_dict))
+  if (!metadata_dict.is_dict()) {
     return false;
+  }
 
-  base::DictionaryValue::Iterator it(*metadata_dict);
-  for (; !it.IsAtEnd(); it.Advance()) {
-    std::string parsed_value;
-    if (!it.value().GetAsString(&parsed_value)) {
-      LOG(ERROR) << "Key \"" << it.key() << "\" did not map to string value";
+  for (const auto& item : metadata_dict.DictItems()) {
+    if (!item.second.is_string()) {
+      LOG(ERROR) << "Key \"" << item.first << "\" did not map to string value";
       return false;
     }
 
-    (*out_metadata)[it.key()] = std::move(parsed_value);
+    (*out_metadata)[item.first] = item.second.GetString();
   }
 
   return true;
@@ -82,121 +81,137 @@ Manifest::Manifest()
 
 bool Manifest::ParseManifest(const std::string& manifest_raw) {
   // Now deserialize the manifest json and read out the rest of the component.
-  int error_code;
-  std::string error_message;
-  JSONStringValueDeserializer deserializer(manifest_raw);
-  std::unique_ptr<base::Value> value =
-      deserializer.Deserialize(&error_code, &error_message);
-
-  if (!value) {
-    LOG(ERROR) << "Could not deserialize the manifest file. Error "
-               << error_code << ": " << error_message;
+  auto manifest_value = base::JSONReader::ReadAndReturnValueWithError(
+      manifest_raw, base::JSON_PARSE_RFC);
+  if (!manifest_value.value) {
+    LOG(ERROR) << "Could not parse the manifest file as JSON. Error "
+               << manifest_value.error_code << ": "
+               << manifest_value.error_message;
     return false;
   }
 
-  base::DictionaryValue* manifest_dict = nullptr;
-  if (!value->GetAsDictionary(&manifest_dict)) {
-    LOG(ERROR) << "Could not parse manifest file as JSON.";
+  if (!manifest_value.value->is_dict()) {
+    LOG(ERROR) << "Manifest file is not dictionary.";
     return false;
   }
+  base::Value manifest_dict = std::move(*manifest_value.value);
 
   // This will have to be changed if the manifest version is bumped.
-  int version;
-  if (!manifest_dict->GetInteger(kManifestVersionField, &version)) {
+  base::Optional<int> manifest_version =
+      manifest_dict.FindIntKey(kManifestVersionField);
+  if (!manifest_version.has_value()) {
     LOG(ERROR) << "Could not parse manifest version field from manifest.";
     return false;
   }
-  if (version != kCurrentManifestVersion) {
+  if (manifest_version != kCurrentManifestVersion) {
     LOG(ERROR) << "Unsupported version of the manifest.";
     return false;
   }
-  manifest_version_ = version;
+  manifest_version_ = *manifest_version;
 
-  std::string image_hash_str;
-  if (!manifest_dict->GetString(kImageHashField, &image_hash_str)) {
+  const std::string* image_hash_str =
+      manifest_dict.FindStringKey(kImageHashField);
+  if (!image_hash_str) {
     LOG(ERROR) << "Could not parse image hash from manifest.";
     return false;
   }
 
-  if (!GetSHA256FromString(image_hash_str, &(image_sha256_))) {
+  if (!GetSHA256FromString(*image_hash_str, &(image_sha256_))) {
     LOG(ERROR) << "Could not convert image hash to bytes.";
     return false;
   }
 
-  std::string table_hash_str;
-  if (!manifest_dict->GetString(kTableHashField, &table_hash_str)) {
+  const std::string* table_hash_str =
+      manifest_dict.FindStringKey(kTableHashField);
+  if (table_hash_str == nullptr) {
     LOG(ERROR) << "Could not parse table hash from manifest.";
     return false;
   }
 
-  if (!GetSHA256FromString(table_hash_str, &(table_sha256_))) {
+  if (!GetSHA256FromString(*table_hash_str, &(table_sha256_))) {
     LOG(ERROR) << "Could not convert table hash to bytes.";
     return false;
   }
 
-  if (!manifest_dict->GetString(kVersionField, &(version_))) {
+  const std::string* version = manifest_dict.FindStringKey(kVersionField);
+  if (!version) {
     LOG(ERROR) << "Could not parse component version from manifest.";
     return false;
   }
+  version_ = *version;
 
   // The fs_type field is optional, and squashfs by default.
-  fs_type_ = FileSystem::kSquashFS;
-  std::string fs_type;
-  if (manifest_dict->GetString(kFSType, &fs_type)) {
-    if (fs_type == "ext4") {
+  const std::string* fs_type = manifest_dict.FindStringKey(kFSType);
+  if (fs_type) {
+    if (*fs_type == "ext4") {
       fs_type_ = FileSystem::kExt4;
-    } else if (fs_type == "squashfs") {
+    } else if (*fs_type == "squashfs") {
       fs_type_ = FileSystem::kSquashFS;
     } else {
-      LOG(ERROR) << "Unsupported file system type: " << fs_type;
+      LOG(ERROR) << "Unsupported file system type: " << *fs_type;
       return false;
     }
+  } else {
+    fs_type_ = FileSystem::kSquashFS;
   }
 
-  if (!manifest_dict->GetBoolean(kIsRemovableField, &is_removable_)) {
-    // If |is-removable| field does not exist, by default it is false.
-    is_removable_ = false;
-  }
+  base::Optional<bool> is_removable =
+      manifest_dict.FindBoolKey(kIsRemovableField);
+  // If |is-removable| field does not exist, by default it is false.
+  is_removable_ = is_removable.value_or(false);
 
-  if (!manifest_dict->GetBoolean(kPreloadAllowed, &preload_allowed_)) {
-    // If |preaload-allowed| field does not exist, by default it is false.
-    preload_allowed_ = false;
-  }
+  base::Optional<bool> preload_allowed =
+      manifest_dict.FindBoolKey(kPreloadAllowed);
+  // If |preaload-allowed| field does not exist, by default it is false.
+  preload_allowed_ = preload_allowed.value_or(false);
 
-  if (!manifest_dict->GetBoolean(kMountFileRequired, &mount_file_required_)) {
-    // If 'mount-file-required' field does not exist, by default it is false.
-    mount_file_required_ = false;
-  }
+  base::Optional<bool> mount_file_required =
+      manifest_dict.FindBoolKey(kMountFileRequired);
+  // If 'mount-file-required' field does not exist, by default it is false.
+  mount_file_required_ = mount_file_required.value_or(false);
 
   // All of these fields are optional.
-  manifest_dict->GetString(kId, &id_);
-  manifest_dict->GetString(kPackage, &package_);
-  manifest_dict->GetString(kName, &name_);
-  manifest_dict->GetString(kImageType, &image_type_);
-  manifest_dict->GetString(kUsedBy, &used_by_);
-  manifest_dict->GetString(kDescription, &description_);
+  const std::string* id = manifest_dict.FindStringKey(kId);
+  if (id)
+    id_ = *id;
+  const std::string* package = manifest_dict.FindStringKey(kPackage);
+  if (package)
+    package_ = *package;
+  const std::string* name = manifest_dict.FindStringKey(kName);
+  if (name)
+    name_ = *name;
+  const std::string* image_type = manifest_dict.FindStringKey(kImageType);
+  if (image_type)
+    image_type_ = *image_type;
+  const std::string* used_by = manifest_dict.FindStringKey(kUsedBy);
+  if (used_by)
+    used_by_ = *used_by;
+  const std::string* description = manifest_dict.FindStringKey(kDescription);
+  if (description)
+    description_ = *description;
 
-  std::string preallocated_size_str;
-  if (manifest_dict->GetString(kPreallocatedSize, &preallocated_size_str)) {
-    if (!base::StringToInt64(preallocated_size_str, &preallocated_size_)) {
+  const std::string* preallocated_size_str =
+      manifest_dict.FindStringKey(kPreallocatedSize);
+  if (preallocated_size_str) {
+    if (!base::StringToInt64(*preallocated_size_str, &preallocated_size_)) {
       LOG(ERROR) << "Manifest pre-allocated-size was malformed: "
-                 << preallocated_size_str;
+                 << *preallocated_size_str;
       return false;
     }
   }
 
-  std::string size_str;
-  if (manifest_dict->GetString(kSize, &size_str)) {
-    if (!base::StringToInt64(size_str, &size_)) {
-      LOG(ERROR) << "Manifest size was malformed: " << size_str;
+  const std::string* size_str = manifest_dict.FindStringKey(kSize);
+  if (size_str) {
+    if (!base::StringToInt64(*size_str, &size_)) {
+      LOG(ERROR) << "Manifest size was malformed: " << *size_str;
       return false;
     }
   }
 
   // Copy out the metadata, if it's there.
-  const base::Value* metadata = nullptr;
-  if (manifest_dict->Get(kMetadataField, &metadata)) {
-    if (!ParseMetadata(metadata, &metadata_)) {
+  const base::Value* metadata = manifest_dict.FindKey(kMetadataField);
+  if (metadata) {
+    if (!ParseMetadata(*metadata, &metadata_)) {
       LOG(ERROR) << "Manifest metadata was malformed";
       return false;
     }
