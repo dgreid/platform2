fc1af2b637609bfcb1068aac7bf90d46c4d66452
hscham <hscham@chromium.org>
2020-10-14 14:27:38 +0900

    cfm-dfu-notification: base::Value refactor
    Replace usages of to-be-deprecated base::DictionaryValue by new
    base::Value API.
    
    BUG=chromium:1099111
    TEST=cros_run_unit_tests --board=eve --packages cfm-dfu-notification
    
    Change-Id: I22b763ffe36a8dd7031af2151387171ede5dad28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2470221
    Tested-by: Grace Cham <hscham@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>

--- cfm-dfu-notification/dfu_log_notification.cc
+++ cfm-dfu-notification/dfu_log_notification.cc
@@ -21,9 +21,9 @@ DfuLogNotification::DfuLogNotification(const std::string& device_name)
     : device_name_(device_name) {}
 
 void DfuLogNotification::NotifyStartUpdate(unsigned int timeout_seconds) {
-  base::DictionaryValue start_parameters;
-  start_parameters.SetString(kNameParameter, device_name_);
-  start_parameters.SetInteger(kTimeoutSecondsParameter, timeout_seconds);
+  base::Value start_parameters(base::Value::Type::DICTIONARY);
+  start_parameters.SetStringKey(kNameParameter, device_name_);
+  start_parameters.SetIntKey(kTimeoutSecondsParameter, timeout_seconds);
 
   std::string start_parameters_json;
   if (base::JSONWriter::Write(start_parameters, &start_parameters_json)) {
@@ -37,9 +37,9 @@ void DfuLogNotification::NotifyStartUpdate(unsigned int timeout_seconds) {
 }
 
 void DfuLogNotification::NotifyEndUpdate(bool success) {
-  base::DictionaryValue end_parameters;
-  end_parameters.SetString(kNameParameter, device_name_);
-  end_parameters.SetBoolean(kSuccessParameter, success);
+  base::Value end_parameters(base::Value::Type::DICTIONARY);
+  end_parameters.SetStringKey(kNameParameter, device_name_);
+  end_parameters.SetBoolKey(kSuccessParameter, success);
 
   std::string end_parameters_json;
   if (base::JSONWriter::Write(end_parameters, &end_parameters_json)) {
@@ -51,9 +51,9 @@ void DfuLogNotification::NotifyEndUpdate(bool success) {
 }
 
 void DfuLogNotification::NotifyUpdateProgress(float percent_done) {
-  base::DictionaryValue progress_parameters;
-  progress_parameters.SetString(kNameParameter, device_name_);
-  progress_parameters.SetDouble(kPercentDoneParameter, percent_done);
+  base::Value progress_parameters(base::Value::Type::DICTIONARY);
+  progress_parameters.SetStringKey(kNameParameter, device_name_);
+  progress_parameters.SetDoubleKey(kPercentDoneParameter, percent_done);
 
   std::string progress_parameters_json;
   if (base::JSONWriter::Write(progress_parameters, &progress_parameters_json)) {
