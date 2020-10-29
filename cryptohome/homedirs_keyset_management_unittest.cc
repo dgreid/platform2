9498357e28b3ea74fd1fa387de390c63039d2100
Daniil Lunev <dlunev@chromium.org>
2020-10-29 19:15:22 +1100

    cryptohome: homedirs_unittest rewrite part 6
    AddKeyset* tests.
    
    BUG=chromium:1141301
    TEST=unittest
    
    Change-Id: I7ef7cd15cd5b321e0635258b7b72ca87449aff8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2507038
    Tested-by: Daniil Lunev <dlunev@chromium.org>
    Commit-Queue: Daniil Lunev <dlunev@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>

--- cryptohome/homedirs_keyset_management_unittest.cc
+++ cryptohome/homedirs_keyset_management_unittest.cc
@@ -19,12 +19,16 @@
 #include "cryptohome/cryptolib.h"
 #include "cryptohome/mock_platform.h"
 #include "cryptohome/mock_tpm.h"
+#include "cryptohome/mock_vault_keyset.h"
+#include "cryptohome/mock_vault_keyset_factory.h"
 #include "cryptohome/vault_keyset.h"
 
 using ::testing::_;
 using ::testing::ElementsAre;
+using ::testing::MatchesRegex;
 using ::testing::NiceMock;
 using ::testing::Return;
+using ::testing::StrEq;
 
 namespace cryptohome {
 
@@ -39,6 +43,8 @@ constexpr char kUser0[] = "First User";
 constexpr char kUserPassword0[] = "user0_pass";
 
 constexpr char kPasswordLabel[] = "password";
+
+constexpr int kInitialKeysetIndex = 0;
 
 }  // namespace
 
@@ -99,9 +105,6 @@ class KeysetManagementTest : public ::testing::Test {
     brillo::SecureBlob passkey;
     cryptohome::Crypto::PasswordToPasskey(password, system_salt_, &passkey);
     Credentials credentials(name, passkey);
-    KeyData key_data;
-    key_data.set_label(kPasswordLabel);
-    credentials.set_key_data(key_data);
 
     UserInfo info = {name,
                      obfuscated,
@@ -121,31 +124,449 @@ class KeysetManagementTest : public ::testing::Test {
       ASSERT_TRUE(platform_.CreateDirectory(user.homedir_path));
     }
   }
+
+  KeyData DefaultKeyData() {
+    KeyData key_data;
+    key_data.set_label(kPasswordLabel);
+    return key_data;
+  }
+
+  void KeysetSetUpWithKeyData(const KeyData& key_data) {
+    for (auto& user : users_) {
+      VaultKeyset vk;
+      vk.Initialize(&platform_, homedirs_.crypto());
+      vk.CreateRandom();
+      *vk.mutable_serialized()->mutable_key_data() = key_data;
+      user.credentials.set_key_data(key_data);
+      ASSERT_TRUE(vk.Encrypt(user.passkey, user.obfuscated));
+      ASSERT_TRUE(
+          vk.Save(user.homedir_path.Append(kKeyFile).AddExtension("0")));
+    }
+  }
+
+  void KeysetSetUpWithoutKeyData() {
+    for (auto& user : users_) {
+      VaultKeyset vk;
+      vk.Initialize(&platform_, homedirs_.crypto());
+      vk.CreateRandom();
+      ASSERT_TRUE(vk.Encrypt(user.passkey, user.obfuscated));
+      ASSERT_TRUE(
+          vk.Save(user.homedir_path.Append(kKeyFile).AddExtension("0")));
+    }
+  }
 };
 
 // Successfully adds initial keyset
 TEST_F(KeysetManagementTest, AddInitialKeyset) {
-  // No key setup to test addition of the first keyset.
+  // SETUP
+
+  users_[0].credentials.set_key_data(DefaultKeyData());
 
   // TEST
 
   EXPECT_TRUE(homedirs_.AddInitialKeyset(users_[0].credentials));
 
   // VERIFY
+  // Initial keyset is added, readable, has "new-er" fields correctly
+  // populated and the initial index is "0".
 
   std::vector<int> indicies;
   EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
-  EXPECT_THAT(indicies, ElementsAre(0));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
 
   VaultKeyset vk0;
   vk0.Initialize(&platform_, homedirs_.crypto());
   EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk0,
                                        /* error */ nullptr));
-  EXPECT_EQ(vk0.legacy_index(), 0);
+  EXPECT_EQ(vk0.legacy_index(), kInitialKeysetIndex);
   EXPECT_EQ(vk0.label(), users_[0].credentials.key_data().label());
   // Expect reset seed and chaps_key to be created.
   EXPECT_TRUE(vk0.serialized().has_wrapped_chaps_key());
   EXPECT_TRUE(vk0.serialized().has_wrapped_reset_seed());
+}
+
+// Successfully adds new keyset
+TEST_F(KeysetManagementTest, AddKeysetSuccess) {
+  // SETUP
+
+  KeysetSetUpWithKeyData(DefaultKeyData());
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+
+  // TEST
+
+  int index = -1;
+  EXPECT_EQ(CRYPTOHOME_ERROR_NOT_SET,
+            homedirs_.AddKeyset(users_[0].credentials, new_passkey, nullptr,
+                                false, &index));
+  EXPECT_NE(index, -1);
+
+  // VERIFY
+  // After we add an additional keyset, we can list and read both of them.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex, index));
+
+  VaultKeyset vk0;
+  vk0.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk0,
+                                       /* error */ nullptr));
+  EXPECT_EQ(vk0.legacy_index(), kInitialKeysetIndex);
+  // We don't have reset seed in the initial generation, so make sure it is
+  // populated on the original key when we add a new one.
+  EXPECT_TRUE(vk0.serialized().has_wrapped_reset_seed());
+
+  VaultKeyset vk1;
+  vk1.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(
+      homedirs_.GetValidKeyset(new_credentials, &vk1, /* error */ nullptr));
+  EXPECT_EQ(vk1.legacy_index(), index);
+}
+
+// Overrides existing keyset on label collision when "clobber" flag is present.
+TEST_F(KeysetManagementTest, AddKeysetClobberSuccess) {
+  // SETUP
+
+  KeysetSetUpWithKeyData(DefaultKeyData());
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+  // Re-use key data from existing credentials to cause label collision.
+  KeyData key_data = users_[0].credentials.key_data();
+  new_credentials.set_key_data(key_data);
+
+  // TEST
+
+  int index = -1;
+  EXPECT_EQ(CRYPTOHOME_ERROR_NOT_SET,
+            homedirs_.AddKeyset(users_[0].credentials, new_passkey, &key_data,
+                                true, &index));
+  EXPECT_EQ(index, 0);
+
+  // VERIFY
+  // When adding new keyset with an "existing" label and the clobber is on, we
+  // expect it to override the keyset with the same label. Thus we shall have
+  // a keyset readable with new_credentials under the index of the old keyset.
+  // The old keyset shall be removed.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
+
+  VaultKeyset vk_old;
+  vk_old.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_FALSE(homedirs_.GetValidKeyset(users_[0].credentials, &vk_old,
+                                        /* error */ nullptr));
+
+  VaultKeyset vk_new;
+  vk_new.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(
+      homedirs_.GetValidKeyset(new_credentials, &vk_new, /* error */ nullptr));
+  EXPECT_EQ(vk_new.legacy_index(), kInitialKeysetIndex);
+}
+
+// Return error on label collision when no "clobber".
+TEST_F(KeysetManagementTest, AddKeysetNoClobber) {
+  // SETUP
+
+  KeysetSetUpWithKeyData(DefaultKeyData());
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+  // Re-use key data from existing credentials to cause label collision.
+  KeyData key_data = users_[0].credentials.key_data();
+  new_credentials.set_key_data(key_data);
+
+  // TEST
+
+  int index = -1;
+  EXPECT_EQ(CRYPTOHOME_ERROR_KEY_LABEL_EXISTS,
+            homedirs_.AddKeyset(users_[0].credentials, new_passkey, &key_data,
+                                false, &index));
+  EXPECT_EQ(index, -1);
+
+  // VERIFY
+  // Label collision without "clobber" causes an addition error. Old keyset
+  // shall still be readable with old credentials, and the new one shall not
+  // exist.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
+
+  VaultKeyset vk_old;
+  vk_old.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk_old,
+                                       /* error */ nullptr));
+  EXPECT_EQ(vk_old.legacy_index(), kInitialKeysetIndex);
+
+  VaultKeyset vk_new;
+  vk_new.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_FALSE(
+      homedirs_.GetValidKeyset(new_credentials, &vk_new, /* error */ nullptr));
+}
+
+// Fail to add new keyset due to invalid label.
+TEST_F(KeysetManagementTest, AddKeysetNonExistentLabel) {
+  // SETUP
+
+  KeysetSetUpWithKeyData(DefaultKeyData());
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+
+  Credentials not_existing_label_credentials = users_[0].credentials;
+  KeyData key_data = users_[0].credentials.key_data();
+  key_data.set_label("i do not exist");
+  not_existing_label_credentials.set_key_data(key_data);
+
+  // TEST
+
+  int index = -1;
+  ASSERT_EQ(CRYPTOHOME_ERROR_AUTHORIZATION_KEY_NOT_FOUND,
+            homedirs_.AddKeyset(not_existing_label_credentials, new_passkey,
+                                nullptr, false, &index));
+  EXPECT_EQ(index, -1);
+
+  // VERIFY
+  // Invalid label causes an addition error. Old keyset shall still be
+  // readable with old credentials, and the new one shall not  exist.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
+
+  VaultKeyset vk_old;
+  vk_old.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk_old,
+                                       /* error */ nullptr));
+  EXPECT_EQ(vk_old.legacy_index(), kInitialKeysetIndex);
+
+  VaultKeyset vk_new;
+  vk_new.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_FALSE(
+      homedirs_.GetValidKeyset(new_credentials, &vk_new, /* error */ nullptr));
+}
+
+// Fail to add new keyset due to invalid credentials.
+TEST_F(KeysetManagementTest, AddKeysetInvalidCreds) {
+  // SETUP
+
+  KeysetSetUpWithKeyData(DefaultKeyData());
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+
+  brillo::SecureBlob wrong_passkey("wrong");
+  Credentials wrong_credentials(users_[0].name, wrong_passkey);
+
+  // TEST
+
+  int index = -1;
+  ASSERT_EQ(CRYPTOHOME_ERROR_AUTHORIZATION_KEY_FAILED,
+            homedirs_.AddKeyset(wrong_credentials, new_passkey, nullptr, false,
+                                &index));
+  EXPECT_EQ(index, -1);
+
+  // VERIFY
+  // Invalid credentials cause an addition error. Old keyset shall still be
+  // readable with old credentials, and the new one shall not  exist.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
+
+  VaultKeyset vk_old;
+  vk_old.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk_old,
+                                       /* error */ nullptr));
+  EXPECT_EQ(vk_old.legacy_index(), kInitialKeysetIndex);
+
+  VaultKeyset vk_new;
+  vk_new.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_FALSE(
+      homedirs_.GetValidKeyset(new_credentials, &vk_new, /* error */ nullptr));
+}
+
+// Fail to add new keyset due to lacking privilieges.
+TEST_F(KeysetManagementTest, AddKeysetInvalidPrivileges) {
+  // SETUP
+
+  KeyData vk_key_data;
+  vk_key_data.mutable_privileges()->set_add(false);
+
+  KeysetSetUpWithKeyData(vk_key_data);
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+
+  // TEST
+
+  int index = -1;
+  ASSERT_EQ(CRYPTOHOME_ERROR_AUTHORIZATION_KEY_DENIED,
+            homedirs_.AddKeyset(users_[0].credentials, new_passkey, nullptr,
+                                false, &index));
+  EXPECT_EQ(index, -1);
+
+  // VERIFY
+  // Invalid permissions cause an addition error. Old keyset shall still be
+  // readable with old credentials, and the new one shall not  exist.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
+
+  VaultKeyset vk_old;
+  vk_old.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk_old,
+                                       /* error */ nullptr));
+  EXPECT_EQ(vk_old.legacy_index(), kInitialKeysetIndex);
+
+  VaultKeyset vk_new;
+  vk_new.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_FALSE(
+      homedirs_.GetValidKeyset(new_credentials, &vk_new, /* error */ nullptr));
+}
+
+// Fail to add new keyset due to index pool exhaustion.
+TEST_F(KeysetManagementTest, AddKeysetNoFreeIndices) {
+  // SETUP
+
+  KeysetSetUpWithKeyData(DefaultKeyData());
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+
+  // Use mock not to literally create a hundread files.
+  EXPECT_CALL(platform_, OpenFile(Property(&base::FilePath::value,
+                                           MatchesRegex(".*/master\\..*$")),
+                                  StrEq("wx")))
+      .WillRepeatedly(Return(nullptr));
+
+  // TEST
+
+  int index = -1;
+  EXPECT_EQ(CRYPTOHOME_ERROR_KEY_QUOTA_EXCEEDED,
+            homedirs_.AddKeyset(users_[0].credentials, new_passkey, nullptr,
+                                false, &index));
+  EXPECT_EQ(index, -1);
+
+  // VERIFY
+  // Nothing should change if we were not able to add keyset due to a lack of
+  // free slots. Since we mocked the "slot" check, we should still have only
+  // initial keyset index, adn the keyset is readable with the old credentials.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
+
+  VaultKeyset vk_old;
+  vk_old.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk_old,
+                                       /* error */ nullptr));
+  EXPECT_EQ(vk_old.legacy_index(), kInitialKeysetIndex);
+
+  VaultKeyset vk_new;
+  vk_new.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_FALSE(
+      homedirs_.GetValidKeyset(new_credentials, &vk_new, /* error */ nullptr));
+}
+
+// Fail to add new keyset due to failed encryption.
+TEST_F(KeysetManagementTest, AddKeysetEncryptFail) {
+  // SETUP
+
+  KeysetSetUpWithoutKeyData();
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+
+  // Mock vk to inject encryption failure
+  MockVaultKeysetFactory vault_keyset_factory;
+  auto mock_vk = new NiceMock<MockVaultKeyset>();
+  mock_vk->mutable_serialized()->set_wrapped_reset_seed("reset_seed");
+  EXPECT_CALL(vault_keyset_factory, New(_, _)).WillOnce(Return(mock_vk));
+  EXPECT_CALL(*mock_vk, Load(_)).WillOnce(Return(true));
+  EXPECT_CALL(*mock_vk, Decrypt(_, _, _)).WillOnce(Return(true));
+  EXPECT_CALL(*mock_vk, Encrypt(new_passkey, _)).WillOnce(Return(false));
+  homedirs_.set_vault_keyset_factory(&vault_keyset_factory);
+
+  // TEST
+
+  int index = -1;
+  ASSERT_EQ(CRYPTOHOME_ERROR_BACKING_STORE_FAILURE,
+            homedirs_.AddKeyset(users_[0].credentials, new_passkey, nullptr,
+                                false, &index));
+  EXPECT_EQ(index, -1);
+
+  // VERIFY
+  // If we failed to save the added keyset due to encryption failure, the old
+  // keyset should still exist and be readable with the old credentials.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
+
+  VaultKeyset vk_old;
+  vk_old.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk_old,
+                                       /* error */ nullptr));
+  EXPECT_EQ(vk_old.legacy_index(), kInitialKeysetIndex);
+
+  VaultKeyset vk_new;
+  vk_new.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_FALSE(
+      homedirs_.GetValidKeyset(new_credentials, &vk_new, /* error */ nullptr));
+}
+
+// Fail to add new keyset due to failed disk write.
+TEST_F(KeysetManagementTest, AddKeysetSaveFail) {
+  // SETUP
+
+  KeysetSetUpWithoutKeyData();
+
+  brillo::SecureBlob new_passkey("new pass");
+  Credentials new_credentials(users_[0].name, new_passkey);
+
+  // Mock vk to inject save failure.
+  MockVaultKeysetFactory vault_keyset_factory;
+  auto mock_vk = new NiceMock<MockVaultKeyset>();
+  mock_vk->mutable_serialized()->set_wrapped_reset_seed("reset_seed");
+  EXPECT_CALL(vault_keyset_factory, New(_, _)).WillOnce(Return(mock_vk));
+  EXPECT_CALL(*mock_vk, Load(_)).WillOnce(Return(true));
+  EXPECT_CALL(*mock_vk, Decrypt(_, _, _)).WillOnce(Return(true));
+  EXPECT_CALL(*mock_vk, Encrypt(new_passkey, _)).WillOnce(Return(true));
+  EXPECT_CALL(*mock_vk, Save(_)).WillOnce(Return(false));
+  homedirs_.set_vault_keyset_factory(&vault_keyset_factory);
+
+  // TEST
+
+  int index = -1;
+  ASSERT_EQ(CRYPTOHOME_ERROR_BACKING_STORE_FAILURE,
+            homedirs_.AddKeyset(users_[0].credentials, new_passkey, nullptr,
+                                false, &index));
+  EXPECT_EQ(index, -1);
+
+  // VERIFY
+  // If we failed to save the added keyset due to disk failure, the old
+  // keyset should still exist and be readable with the old credentials.
+
+  std::vector<int> indicies;
+  EXPECT_TRUE(homedirs_.GetVaultKeysets(users_[0].obfuscated, &indicies));
+  EXPECT_THAT(indicies, ElementsAre(kInitialKeysetIndex));
+
+  VaultKeyset vk_old;
+  vk_old.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_TRUE(homedirs_.GetValidKeyset(users_[0].credentials, &vk_old,
+                                       /* error */ nullptr));
+  EXPECT_EQ(vk_old.legacy_index(), kInitialKeysetIndex);
+
+  VaultKeyset vk_new;
+  vk_new.Initialize(&platform_, homedirs_.crypto());
+  EXPECT_FALSE(
+      homedirs_.GetValidKeyset(new_credentials, &vk_new, /* error */ nullptr));
 }
 
 }  // namespace cryptohome
