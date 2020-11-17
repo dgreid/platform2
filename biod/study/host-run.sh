3e2e97f09af070ceffb9a65e8875c69b67310e44
Craig Hesling <hesling@chromium.org>
2020-11-17 11:47:35 -0800

    fingerprint_study: Add dir independence to host-run.sh
    This allows you to run host-run.sh from any PWD.
    This also adds basic error handling functionality.
    
    BUG=b:144061947
    TEST=Ran host-run.sh from outside study dir
    TEST=Ran host-run.sh in env where pip3 and virtualenv don't exist.
    
    Change-Id: Iecbf9982a24150a8b47f6dab52fa8b05d77bfaa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2545186
    Tested-by: Craig Hesling <hesling@chromium.org>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Queue: Craig Hesling <hesling@chromium.org>

--- biod/study/host-run.sh
+++ biod/study/host-run.sh
@@ -16,19 +16,37 @@ LOG_DIR=
 
 FPSTUDY_VIRTENV=/tmp/virtualenv-study
 
+# Check for required commands.
+CMDS_REQUIRED=( pip3 virtualenv )
+if ! which "${CMDS_REQUIRED[@]}" >/dev/null 2>&1; then
+  echo "Error - Missing one or more required commands: ${CMDS_REQUIRED[*]}" >&2
+  exit 1
+fi
+
+# Find the fingerprint study base directory.
+study_dir="$(dirname "${BASH_SOURCE[0]}")"
+
 # Setup New Virtualenv
 rm -rf "${FPSTUDY_VIRTENV}"
-virtualenv -p python3 "${FPSTUDY_VIRTENV}"
+if ! virtualenv -p python3 "${FPSTUDY_VIRTENV}"; then
+  echo "Error - Failed to setup a python virtualenv." >&2
+  exit 1
+fi
+# shellcheck source=/dev/null
 . "${FPSTUDY_VIRTENV}/bin/activate"
-pip3 install -r requirements.txt
+if ! pip3 install -r "${study_dir}/requirements.txt"; then
+  echo "Error - Failed to install python dependencies." >&2
+  exit 1
+fi
 
 if [[ -n "${LOG_DIR}" ]]; then
   mkdir -p "${LOG_DIR}"
 fi
-PATH="$(pwd)/mock-bin:${PATH}" ./study_serve.py  \
-  --finger_count="${FINGER_COUNT}"               \
-  --enrollment_count="${ENROLLMENT_COUNT}"       \
-  --verification_count="${VERIFICATION_COUNT}"   \
-  --picture_dir="${PICTURE_DIR}"                 \
-  --log_dir="${LOG_DIR}"                         \
+
+PATH="${study_dir}/mock-bin:${PATH}" "${study_dir}/study_serve.py"             \
+  --finger_count="${FINGER_COUNT}"                                             \
+  --enrollment_count="${ENROLLMENT_COUNT}"                                     \
+  --verification_count="${VERIFICATION_COUNT}"                                 \
+  --picture_dir="${PICTURE_DIR}"                                               \
+  --log_dir="${LOG_DIR}"                                                       \
   "$@"
