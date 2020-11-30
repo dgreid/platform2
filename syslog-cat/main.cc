c4ac5f9eaadd1e53d3aece9a387314781dbc9bdc
Yoshiki Iguchi <yoshiki@google.com>
2020-12-01 04:17:45 +0900

    syslog-cat: Add tests and refactor code
    This CL adds tests for syslog-cat and refactor the code for it.
    Also this CL fixes bugs and nits which are mentioned in
    the comments in crrev.com/c/2371542.
    
    BUG=chromium:1128630
    TEST=Ran added tests
    
    Cq-Depend: chromium:2567202
    Change-Id: I36535e70ad5be0033d1650fd15a8148bb4ca23e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2567284
    Tested-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Yoshiki Iguchi <yoshiki@chromium.org>

--- syslog-cat/main.cc
+++ syslog-cat/main.cc
@@ -2,212 +2,115 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stdio.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/un.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
 #include <string>
 #include <vector>
 
 #include <base/command_line.h>
-#include <base/strings/string_piece.h>
 #include <base/files/file_path.h>
-#include <base/files/file_util.h>
-#include <base/files/scoped_file.h>
 #include <base/logging.h>
 #include <base/process/process.h>
-#include <base/posix/eintr_wrapper.h>
-#include <base/strings/stringprintf.h>
-#include <base/strings/string_number_conversions.h>
 #include <base/strings/string_util.h>
 
+#include <brillo/flag_helper.h>
+
+#include "syslog-cat/syslogcat.h"
+
 namespace {
-constexpr int kDefaultSeverityStdout = 6;
-constexpr int kDefaultSeverityStderr = 4;
+constexpr char kDefaultSeverityStdout[] = "info";
+constexpr char kDefaultSeverityStderr[] = "warn";
 constexpr char kSyslogSocketPath[] = "/run/rsyslogd/stdout";
 
-void ShowUsage() {
-  fprintf(
-      stderr,
-      "Usage: syslog_cat [OPTION] -- target-command arguments...\n"
-      "  options:\n"
-      "    --identifier=IDENTIFIER     specify the identifier of log.\n"
-      "    --severity-stdout=PRIORITY  specify the severity of log from\n"
-      "                                stdout. PRIORITY is a number 0-8.\n"
-      "    --severity-stderr=PRIORITY  specify the severity of log from\n"
-      "                                stderr. PRIORITY is a number 0-8.\n");
-}
-
-base::ScopedFD PrepareSocket(const std::string& identifier,
-                             int severity,
-                             int pid) {
-  DCHECK(!identifier.empty());
-  DCHECK_GE(severity, 0);
-  DCHECK_LE(severity, 7);
-
-  // Open the unix socket to write logs.
-  base::ScopedFD sock(HANDLE_EINTR(socket(AF_UNIX, SOCK_STREAM, 0)));
-  if (!sock.is_valid()) {
-    PLOG(ERROR) << "opening stream socket";
-    return base::ScopedFD();
-  }
-
-  // Connect the syslog unix socket file.
-  struct sockaddr_un server {};
-  server.sun_family = AF_UNIX;
-  std::strncpy(server.sun_path, kSyslogSocketPath, sizeof(kSyslogSocketPath));
-  if (HANDLE_EINTR(connect(sock.get(), (struct sockaddr*)&server,
-                           sizeof(struct sockaddr_un))) < 0) {
-    PLOG(ERROR) << "connecting stream socket";
-    return base::ScopedFD();
-  }
-
-  // Construct the header string to send.
-  std::string header = base::StringPrintf("TAG=%s[%d]\nPRIORITY=%d\n\n",
-                                          identifier.c_str(), pid, severity);
-
-  // Send headers (tag and severity).
-  if (!base::WriteFileDescriptor(sock.get(), header.c_str(), header.size())) {
-    PLOG(ERROR) << "writing headers on stream socket";
-    return base::ScopedFD();
-  }
-
-  return sock;
-}
-
-int SeverityFromString(base::StringPiece severity_str) {
-  if (severity_str == "0" ||
-      base::CompareCaseInsensitiveASCII(severity_str, "emerg") == 0) {
+int SeverityFromString(const base::StringPiece& severity) {
+  std::string severity_lower = base::ToLowerASCII(severity);
+  if (severity_lower == "0" || severity_lower == "emerg") {
     return 0;
   }
 
-  if (severity_str == "1" ||
-      base::CompareCaseInsensitiveASCII(severity_str, "alert") == 0) {
+  if (severity_lower == "1" || severity_lower == "alert") {
     return 1;
   }
 
-  if (severity_str == "2" ||
-      base::CompareCaseInsensitiveASCII(severity_str, "critical") == 0 ||
-      base::CompareCaseInsensitiveASCII(severity_str, "crit") == 0) {
+  if (severity_lower == "2" || severity_lower == "critical" ||
+      severity_lower == "crit") {
     return 2;
   }
 
-  if (severity_str == "3" ||
-      base::CompareCaseInsensitiveASCII(severity_str, "err") == 0 ||
-      base::CompareCaseInsensitiveASCII(severity_str, "error") == 0) {
+  if (severity_lower == "3" || severity_lower == "err" ||
+      severity_lower == "error") {
     return 3;
   }
 
-  if (severity_str == "4" ||
-      base::CompareCaseInsensitiveASCII(severity_str, "warn") == 0 ||
-      base::CompareCaseInsensitiveASCII(severity_str, "warning") == 0) {
+  if (severity_lower == "4" || severity_lower == "warn" ||
+      severity_lower == "warning") {
     return 4;
   }
 
-  if (severity_str == "5" ||
-      base::CompareCaseInsensitiveASCII(severity_str, "notice") == 0) {
+  if (severity_lower == "5" || severity_lower == "notice") {
     return 5;
   }
 
-  if (severity_str == "6" ||
-      base::CompareCaseInsensitiveASCII(severity_str, "info") == 0) {
+  if (severity_lower == "6" || severity_lower == "info") {
     return 6;
   }
 
-  if (severity_str == "7" ||
-      base::CompareCaseInsensitiveASCII(severity_str, "debug") == 0) {
+  if (severity_lower == "7" || severity_lower == "debug") {
     return 7;
   }
 
   return -1;
 }
 
-// Extract a severity from the command line argument.
-// Return the default severity if the argument is not specified
-// Return "-1" if an invalid value is specified.
-int ExtractSeverityFromCommnadLine(const base::CommandLine* command_line,
-                                   const char* switch_name,
-                                   int default_severity) {
-  if (!command_line->HasSwitch(switch_name))
-    return default_severity;
-
-  return SeverityFromString(command_line->GetSwitchValueASCII(switch_name));
-}
-
-bool CreateSocketAndBindToFD(const std::string& identifier,
-                             int severity,
-                             int pid,
-                             int target_fd) {
-  base::ScopedFD sock = PrepareSocket(identifier, severity, pid);
-  if (!sock.is_valid()) {
-    LOG(ERROR) << "Failed to open the rsyslog socket for stderr.";
-    return false;
-  }
-
-  // Connect the socket to stderr.
-  if (HANDLE_EINTR(dup2(sock.get(), target_fd)) == -1) {
-    PLOG(ERROR) << "duping the stderr";
-    return false;
-  }
-
-  return true;
-}
-
 }  // namespace
 
 int main(int argc, char* argv[]) {
-  base::CommandLine::Init(argc, argv);
+  DEFINE_string(identifier, "", "Identifier string of syslog");
+  DEFINE_string(severity_stdout, kDefaultSeverityStdout,
+                "Severity value which is used in sendin stdout to syslog");
+  DEFINE_string(severity_stderr, kDefaultSeverityStderr,
+                "Severity value which is used in sendin stderr to syslog");
 
-  const base::CommandLine* command_line =
-      base::CommandLine::ForCurrentProcess();
-  const auto& sv = command_line->GetArgs();
+  brillo::FlagHelper::Init(
+      argc, argv,
+      "Captures the stdout/stderr of the specified program and forward them "
+      "to syslog.");
 
-  if (sv.size() == 0 || command_line->HasSwitch("help")) {
-    ShowUsage();
+  const auto& sv = base::CommandLine::ForCurrentProcess()->GetArgs();
+  if (sv.size() == 0) {
+    LOG(ERROR) << "Syslog-cat requres the command line to execute.";
     return 1;
   }
 
+  const std::string& target_command = sv[0];
+
   // Prepare a identifier.
-  constexpr char kIdentifierSwitchName[] = "identifier";
-  std::string identifier;
-  if (command_line->HasSwitch(kIdentifierSwitchName)) {
-    identifier = command_line->GetSwitchValueASCII(kIdentifierSwitchName);
-  } else {
-    const std::string& target_command_str = sv[0].c_str();
-    const base::FilePath& target_command_path =
-        base::FilePath(target_command_str);
-    identifier = target_command_path.BaseName().value();
+  std::string identifier = FLAGS_identifier;
+  if (identifier.empty()) {
+    // Fallback to the default.
+    identifier = base::FilePath(target_command).BaseName().value();
   }
   if (identifier.empty()) {
+    // Failed to fallback for some reason.
     LOG(ERROR) << "Failed to extract a identifier string.";
     return 1;
   }
 
   // Prepare a severity for stdout.
-  constexpr char kSeverityOutSwitchName[] = "severity-stdout";
-  int severity_stdout = ExtractSeverityFromCommnadLine(
-      command_line, kSeverityOutSwitchName, kDefaultSeverityStdout);
+  int severity_stdout = SeverityFromString(FLAGS_severity_stdout);
   if (severity_stdout < 0) {
-    LOG(ERROR) << "Invalid severity value. It must be a number between "
-               << "0 (EMERG) and 7 (DEBUG) or valid severity string.";
+    LOG(ERROR) << "Invalid --severity_stdout value '" << severity_stdout
+               << "'. It must be a number between 0 (EMERG) and 7 (DEBUG) or "
+                  "valid severity string.";
     return 1;
   }
 
   // Prepare a severity for stderr.
-  constexpr char kSeverityErrSwitchName[] = "severity-stderr";
-  int severity_stderr = ExtractSeverityFromCommnadLine(
-      command_line, kSeverityErrSwitchName, kDefaultSeverityStderr);
+  int severity_stderr = SeverityFromString(FLAGS_severity_stderr);
   if (severity_stderr < 0) {
-    LOG(ERROR) << "Invalid severity value. It must be a number between "
-               << "0 (EMERG) and 7 (DEBUG) or valid severity string.";
+    LOG(ERROR) << "Invalid --severity_stderr value '" << severity_stderr
+               << "'. It must be a number between 0 (EMERG) and 7 (DEBUG) or "
+                  "valid severity string.";
     return 1;
   }
-
-  // Prepare a pid.
-  base::ProcessId pid = base::Process::Current().Pid();
 
   // Prepare a command line for the target process.
   int target_command_argc = sv.size();
@@ -216,25 +119,13 @@ int main(int argc, char* argv[]) {
     target_command_argv[i] = sv[i].c_str();
   target_command_argv[target_command_argc] = nullptr;
 
-  // Open the unix socket to redirect logs from stdout (and maybe stderr).
-  bool ret_stdout =
-      CreateSocketAndBindToFD(identifier, severity_stdout, pid, STDOUT_FILENO);
-  CHECK(ret_stdout) << "Failed to bind stdout.";
+  ExecuteCommandWithRedirection(target_command, target_command_argv, identifier,
+                                severity_stdout, severity_stderr,
+                                base::FilePath(kSyslogSocketPath),
+                                base::FilePath(kSyslogSocketPath));
 
-  // Open the unix socket to redirect logs from stderr.
-  // We prepare a sock for stderr even if the severarities are same, in order to
-  // prevent interleave of simultaneous lines.
-  bool ret_stderr =
-      CreateSocketAndBindToFD(identifier, severity_stderr, pid, STDERR_FILENO);
-  CHECK(ret_stderr) << "Failed to bind stderr.";
+  // The method above should never return unless an error happens.
 
-  // Execute the target process.
-  execvp(const_cast<char*>(sv[0].c_str()),
-         const_cast<char**>(target_command_argv.data()));
-
-  /////////////////////////////////////////////////////////////////////////////
-  // The code below should not be executed if the execvp() above succeeds.
-
-  PLOG(ERROR) << "execvp '" << sv[0] << "'";
-  exit(1);
+  // Crashes itself by the FATAL error message.
+  LOG(FATAL) << "Executing the command is unexpectedly failed.";
 }
