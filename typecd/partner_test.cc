04dd223a72a0daefcdce23f2454d06598d962d1b
Prashant Malani <pmalani@chromium.org>
2020-12-05 00:43:41 -0800

    typecd: Use ScopedTempDir in PartnerTest
    Use ScopedTempDir instead of a regular temporary directory in
    PartnerTest test fixtures. This ensures that the files created for the
    tests get cleaned up correctly.
    
    BUG=None
    TEST=Make sure typecd unit tests still pass.
    
    Change-Id: I2abd0e4c2ab9e0c49415cea266afcebbe30577fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2576164
    Reviewed-by: Prashant Malani <pmalani@chromium.org>
    Reviewed-by: Mengqi Guo <mqg@chromium.org>
    Commit-Queue: Prashant Malani <pmalani@chromium.org>
    Tested-by: Prashant Malani <pmalani@chromium.org>

--- typecd/partner_test.cc
+++ typecd/partner_test.cc
@@ -6,6 +6,7 @@
 
 #include <string>
 
+#include <base/files/scoped_temp_dir.h>
 #include <base/strings/stringprintf.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -25,7 +26,17 @@ const uint32_t kPartnerPDIdHeaderVDO = 0x12341234;
 
 namespace typecd {
 
-class PartnerTest : public ::testing::Test {};
+class PartnerTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    ASSERT_TRUE(scoped_temp_dir_.CreateUniqueTempDir());
+    temp_dir_ = scoped_temp_dir_.GetPath();
+  }
+
+ public:
+  base::FilePath temp_dir_;
+  base::ScopedTempDir scoped_temp_dir_;
+};
 
 // Check that calls of AddAltMode() done explicitly function correctly. Also
 // check that trying to Add the same alt mode twice fails.
@@ -33,19 +44,16 @@ TEST_F(PartnerTest, TestAltModeManualAddition) {
   Partner p((base::FilePath(kFakePort0PartnerSysPath)));
 
   // Set up fake sysfs paths.
-  base::FilePath temp_dir;
-  ASSERT_TRUE(base::CreateNewTempDirectory("", &temp_dir));
-
   std::string mode0_dirname =
       base::StringPrintf("port%d-partner.%d", 0, kDPAltModeIndex);
-  auto mode0_path = temp_dir.Append(mode0_dirname);
+  auto mode0_path = temp_dir_.Append(mode0_dirname);
   ASSERT_TRUE(CreateFakeAltMode(mode0_path, kDPSVID, kDPVDO, kDPVDOIndex));
 
   EXPECT_TRUE(p.AddAltMode(mode0_path));
 
   std::string mode1_dirname =
       base::StringPrintf("port%d-partner.%d", 0, kTBTAltModeIndex);
-  auto mode1_path = temp_dir.Append(mode1_dirname);
+  auto mode1_path = temp_dir_.Append(mode1_dirname);
   ASSERT_TRUE(CreateFakeAltMode(mode1_path, kTBTSVID, kTBTVDO, kTBTVDOIndex));
 
   // Add extra white spaces to ensure malformed strings can be parsed. We can do
@@ -67,10 +75,7 @@ TEST_F(PartnerTest, TestAltModeManualAddition) {
 // gets parsed and stored correctly.
 TEST_F(PartnerTest, TestPDIdentityScan) {
   // Set up fake sysfs paths.
-  base::FilePath temp_dir;
-  ASSERT_TRUE(base::CreateNewTempDirectory("", &temp_dir));
-
-  auto partner_path = temp_dir.Append(std::string("port0-partner"));
+  auto partner_path = temp_dir_.Append(std::string("port0-partner"));
   ASSERT_TRUE(base::CreateDirectory(partner_path));
 
   auto identity_path = partner_path.Append(std::string("identity"));
