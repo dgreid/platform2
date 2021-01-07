3efcd339bc28524e57429a30e6082381395ac6b8
Amin Hassani <ahassani@chromium.org>
2021-01-06 19:23:36 -0800

    installer: Migrate flag processing of cros_installer to brillo:FlagHelper
    This makes it easier to add flags related to chromeos-install here
    easily.
    
    Since we call chromeos-postinst of the newly installed image as a
    mounted file system, it is fine to edit this file without breaking
    anything (as long as it is still a shell script).
    
    BUG=b:176492189
    TEST=chromeos-install # through USB
    TEST=cros flash # of newly built image.
    
    Change-Id: Ia84da90a41f01ad4e4cdd57e699a356225eb0547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2614125
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Commit-Queue: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- installer/cros_installer_main.cc
+++ installer/cros_installer_main.cc
@@ -2,95 +2,46 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <string>
+#include <brillo/flag_helper.h>
 
 #include "installer/chromeos_install_config.h"
-#include "installer/chromeos_legacy.h"
 #include "installer/chromeos_postinst.h"
-#include "installer/inst_util.h"
 
-using std::string;
+int main(int argc, char* argv[]) {
+  DEFINE_string(type, "", "Install type, one of: postinst.");
 
-constexpr char usage[] =
-    "cros_installer:\n"
-    "   --help\n"
-    "   --debug\n"
-    "   cros_installer postinst <install_dev> <mount_point> [ args ]\n"
-    "     --bios [ secure | legacy | efi | uboot ]\n";
+  // postinst flags.
+  DEFINE_string(bios, "", "Bios type, one of: secure, legacy, efi, and uboot.");
+  DEFINE_string(install_dev, "", "Install device. e.g. /");
+  DEFINE_string(install_dir, "", "Install directory. e.g. /tmp/blah");
 
-int showHelp() {
-  printf("%s", usage);
-  return 1;
-}
+  brillo::FlagHelper::Init(argc, argv, "cros_installer");
 
-int main(int argc, char** argv) {
-  struct option long_options[] = {
-      {"bios", required_argument, NULL, 'b'},
-      {"help", no_argument, NULL, 'h'},
-      {NULL, 0, NULL, 0},
-  };
-
-  // Unkown means we will attempt to autodetect later on.
-  BiosType bios_type = kBiosTypeUnknown;
-
-  while (true) {
-    int option_index;
-    int c = getopt_long(argc, argv, "h", long_options, &option_index);
-
-    if (c == -1)
-      break;
-
-    switch (c) {
-      case '?':
-        // Unknown argument
-        printf("\n");
-        return showHelp();
-
-      case 'h':
-        // --help
-        return showHelp();
-
-      case 'b':
-        // Bios type has been explicitly given, don't autodetect
-        if (!StrToBiosType(optarg, &bios_type))
-          return 1;
-        break;
-
-      default:
-        LOG(INFO) << "Unknown argument " << c
-                  << " - switch and struct out of sync.";
-        return showHelp();
+  if (FLAGS_type == "postinst") {
+    // Unknown means we will attempt to autodetect later on.
+    BiosType bios_type = kBiosTypeUnknown;
+    if (!FLAGS_bios.empty() && !StrToBiosType(FLAGS_bios, &bios_type)) {
+      LOG(ERROR) << "Invalid bios type: " << FLAGS_bios;
+      return 1;
     }
-  }
-
-  if (argc - optind < 1) {
-    LOG(INFO) << "No command type present (postinst, etc).";
-    return showHelp();
-  }
-
-  string command = argv[optind++];
-
-  // Run postinstall behavior
-  if (command == "postinst") {
-    if (argc - optind != 2)
-      return showHelp();
-
-    string install_dir = argv[optind++];
-    string install_dev = argv[optind++];
+    if (FLAGS_install_dev.empty()) {
+      LOG(ERROR) << "--install_dev is empty.";
+      return 1;
+    }
+    if (FLAGS_install_dir.empty()) {
+      LOG(ERROR) << "--install_dir is empty.";
+      return 1;
+    }
 
     int exit_code = 0;
-    if (!RunPostInstall(install_dev, install_dir, bios_type, &exit_code)) {
-      if (!exit_code)
-        exit_code = 1;
+    if (!RunPostInstall(FLAGS_install_dev, FLAGS_install_dir, bios_type,
+                        &exit_code)) {
+      return exit_code ? exit_code : 1;
     }
-
-    return exit_code;
+  } else {
+    LOG(ERROR) << "Invalid --type flag is passed.";
+    return 1;
   }
 
-  LOG(INFO) << "Unknown command: " << command;
-  return showHelp();
+  return 0;
 }
