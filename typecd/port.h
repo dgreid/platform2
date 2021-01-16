b1b686a40d00b954017a754fd8aa85c239d8233c
caveh jalali <caveh@chromium.org>
2021-01-16 20:32:27 +0000

    Revert "typecd: Add MockPort for unit tests"
    This reverts commit e0b2c8b538f238e9788d0d5a3f18f5edcdc5ad57.
    
    Reason for revert:
    
    failed on builder:
    
    typecd-0.0.1-r112: ../../../../../../../tmp/portage/chromeos-base/typecd-0.0.1-r112/work/typecd-0.0.1/platform2/typecd/port_manager_test.cc:52:38: error: use of undeclared identifier 'TYPEC_MODE_DP'
    typecd-0.0.1-r112:   EXPECT_CALL(*ec_util, EnterMode(0, TYPEC_MODE_DP))
    
    https://ci.chromium.org/ui/p/chromeos/builders/postsubmit/volteer-snapshot/4857/overview
    
    
    TYPEC_MODE_DP was renamed in
    https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2631369
    which landed in 13731.0.0
    
    but
    https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2632226
    landed in 13732.0.0 but uses the old name which
    should have failed in the CQ.
    
    Exempt-From-Owner-Approval: broke CQ
    
    Original change's description:
    > typecd: Add MockPort for unit tests
    >
    > Add a MockPort class which derives from a regular Port class. With this,
    > we can control the behaviour of the class (which modes it supports,
    > whether cable discovery is complete, etc.) without having to set up an
    > entire fake sysfs directory and really adding Cable and Partner objects.
    > This in turn allows to test out various scenarios of the RunModeEntry()
    > code.
    >
    > Make the relevant changes to the Port class to allow the MockPort to be
    > defined.
    >
    > Also, add a simple unit test that uses a MockPort which can only enter
    > DP, and validate that the ECUtil function is called correctly.
    >
    > BUG=b:168653671
    > TEST=1. Check that all unit tests still pass.
    >      2. Build and boot on a voxel. Validate that partner, cable and alt
    >      mode hotplug removal events are being generated as expected in
    >      typecd.log
    >
    > Change-Id: I1196144582ed2ee81b9c6fed0db0c1cf7ead02f0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2632226
    > Reviewed-by: Mengqi Guo <mqg@chromium.org>
    > Tested-by: Prashant Malani <pmalani@chromium.org>
    > Commit-Queue: Prashant Malani <pmalani@chromium.org>
    
    Bug: b:168653671
    Change-Id: Ia88646423b555e8960073724317a221a12a3c21c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2634622
    Tested-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: caveh jalali <caveh@chromium.org>
    Commit-Queue: caveh jalali <caveh@chromium.org>

--- typecd/port.h
+++ typecd/port.h
@@ -25,7 +25,6 @@ class Port {
  public:
   static std::unique_ptr<Port> CreatePort(const base::FilePath& syspath);
   Port(const base::FilePath& syspath, int port_num);
-  virtual ~Port() = default;
 
   void AddPartner(const base::FilePath& path);
   void RemovePartner();
@@ -47,37 +46,37 @@ class Port {
 
   // Read and return the current port data role from sysfs.
   // Returns either "host" or "device" on success, empty string on failure.
-  virtual std::string GetDataRole();
+  std::string GetDataRole();
 
   // Check whether we can enter DP Alt Mode. This should check for the presence
   // of required attributes on the Partner and (if applicable) Cable.
-  virtual bool CanEnterDPAltMode();
+  bool CanEnterDPAltMode();
 
   // Check whether we can enter Thunderbolt Compatibility Alt Mode. This should
   // check for the presence of required attributes on the Partner and
   // (if applicable) Cable.
-  virtual bool CanEnterTBTCompatibilityMode();
+  bool CanEnterTBTCompatibilityMode();
 
   // Returns whether the partner can enter USB4. This should check the following
   // attributes for USB4 support:
   // - Partner(SOP) PD identity.
   // - Cable speed.
   // - Cable type.
-  virtual bool CanEnterUSB4();
+  bool CanEnterUSB4();
 
   // Returns true when all PD discovery information (PD Identity VDOs, all
   // Discover Mode data) for a partner has been processed.
   //
   // NOTE: Any mode entry decision logic should only run if this function
   // returns true.
-  virtual bool IsPartnerDiscoveryComplete();
+  bool IsPartnerDiscoveryComplete();
 
   // Return true when all PD discovery information (PD Identity VDOs, all
   // Discover Mode data) for a cable has been processed.
   //
   // NOTE: Any mode entry decision logic should only run if this function
   // returns true.
-  virtual bool IsCableDiscoveryComplete();
+  bool IsCableDiscoveryComplete();
 
  private:
   friend class PortTest;
--- typecd/port_manager_test.cc
+++ typecd/port_manager_test.cc
@@ -4,13 +4,10 @@
 
 #include "typecd/port_manager.h"
 
-#include <string>
-
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "typecd/mock_ec_util.h"
-#include "typecd/mock_port.h"
 
 using ::testing::_;
 using ::testing::Return;
@@ -36,43 +33,6 @@ TEST_F(PortManagerTest, ModeEntryNotSupported) {
 
   // It doesn't matter that we haven't registered any ports, since the code
   // should return before this is checked.
-  port_manager->RunModeEntry(0);
-
-  // There is no explicit test here, just that the Mock expectations should be
-  // met.
-}
-
-// Test the basic case of DP mode entry.
-TEST_F(PortManagerTest, SimpleModeEntry) {
-  auto port_manager = std::make_unique<PortManager>();
-
-  // Create the MockECUtil and set the expectations (enter DP called once).
-  auto ec_util = std::make_unique<MockECUtil>();
-  EXPECT_CALL(*ec_util, ModeEntrySupported()).Times(0);
-  EXPECT_CALL(*ec_util, EnterMode(0, TYPEC_MODE_DP))
-      .WillOnce(testing::Return(true));
-  EXPECT_CALL(*ec_util, ExitMode(_)).Times(0);
-  port_manager->SetECUtil(ec_util.get());
-
-  // Add a fake port that supports only DP.
-  auto port = std::make_unique<MockPort>(base::FilePath("fakepath"), 0);
-  EXPECT_CALL(*port, GetDataRole())
-      .WillRepeatedly(testing::Return(std::string("host")));
-  EXPECT_CALL(*port, IsPartnerDiscoveryComplete())
-      .WillRepeatedly(testing::Return(true));
-  EXPECT_CALL(*port, IsCableDiscoveryComplete())
-      .WillRepeatedly(testing::Return(true));
-  EXPECT_CALL(*port, CanEnterUSB4()).WillRepeatedly(testing::Return(false));
-  EXPECT_CALL(*port, CanEnterTBTCompatibilityMode())
-      .WillRepeatedly(testing::Return(false));
-  EXPECT_CALL(*port, CanEnterDPAltMode()).WillRepeatedly(testing::Return(true));
-  port_manager->ports_.insert(
-      std::pair<int, std::unique_ptr<Port>>(0, std::move(port)));
-
-  // Since we only have a MockECUtil, just force the |mode_entry_supported_|
-  // flag.
-  port_manager->SetModeEntrySupported(true);
-
   port_manager->RunModeEntry(0);
 
   // There is no explicit test here, just that the Mock expectations should be
